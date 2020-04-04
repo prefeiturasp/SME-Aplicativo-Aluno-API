a4a1ebce8185cf3ab80eb46c267480ef94e8bc51
alexandreamcom <alexandre.novaes@amcom.com.br>
2020-04-04 15:16:40 -0300

    AB#14217

---
+++ src/SME.AE.Infra/Persistencia/AutenticacaoRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using SME.AE.Aplicacao.Comum.Config;
+using SME.AE.Aplicacao.Comum.Interfaces;
+using SME.AE.Infra.Persistencia.Consultas;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace SME.AE.Infra.Persistencia
+{
+    public class AutenticacaoRepositorio : IAutenticacaoRepositorio
+    {
+        private readonly string whereReponsavelAluno = "@ WHERE responsalvel.cpf = @cpf " +
+            "AND aluno.dt_nascimento_aluno = @dataNascimentoAluno";
+        public async Task<bool> ValidarUsuarioEol(string cpf, DateTime dataNascimentoAluno)
+        {
+            using var conexao = new SqlConnection(ConnectionStrings.ConexaoEol);
+            var aluno = await conexao.QueryFirstAsync<string>($"{AutenticacaoConsultas.ObterAlunosDoResponsavel}{whereReponsavelAluno}", (cpf, dataNascimentoAluno));
+            return string.IsNullOrEmpty(aluno);
+        }
+    }
+}
