4c5d5ddbec6cee4b893b624af2daef73c741a584
Caique Siqueira <latorrecaique@gmail.com>
2020-04-15 11:23:49 -0300

    AB#14992 Romver por id e remover em lote

--- src/SME.AE.Aplicacao/Comandos/Autenticacao/CriarUsuario/Notificacao/Remover/RemoverNotificacaoCommand.cs
+++ src/SME.AE.Aplicacao/Comandos/Autenticacao/CriarUsuario/Notificacao/Remover/RemoverNotificacaoCommand.cs
@@ -1,21 +1,25 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using SME.AE.Aplicacao.Comum.Modelos;
 
 namespace SME.AE.Aplicacao.Comandos.Notificacao.Remover
 {
-    public class RemoverNotificacaoCommand : IRequest<bool>
+    public class RemoverNotificacaoCommand : IRequest<string>
     {
-        public Dominio.Entidades.Notificacao Notificacao { get; set; }
+        public long[] Ids { get; set; }
 
-        public RemoverNotificacaoCommand(Dominio.Entidades.Notificacao notificacao)
+        public RemoverNotificacaoCommand(long[] ids)
         {
-            Notificacao = notificacao;
+            Ids = ids;
         }
     }
 
-    public class RemoverNotificacaoCommandHandler : IRequestHandler<RemoverNotificacaoCommand, bool>
+    public class RemoverNotificacaoCommandHandler : IRequestHandler<RemoverNotificacaoCommand, string>
     {
         private readonly INotificacaoRepository _repository;
         
@@ -24,9 +28,31 @@ namespace SME.AE.Aplicacao.Comandos.Notificacao.Remover
             _repository = repository;
         }
 
-        public async Task<bool> Handle(RemoverNotificacaoCommand request, CancellationToken cancellationToken)
+
+        public async Task<string> Handle(RemoverNotificacaoCommand request, CancellationToken cancellationToken)
         {
-            return await _repository.Remover(request.Notificacao);
+            var erros = new StringBuilder();
+            foreach (long id in request.Ids)
+            {
+                var notificacao =  await _repository.ObterPorId(id);
+                if (notificacao == null)
+                {
+                    erros.Append($"<li>{id} - comunicado não encontrado</li>");
+                }
+                    
+                else
+                {
+                    try
+                    {
+                        await _repository.Remover(notificacao);
+                    }
+                    catch
+                    {
+                        erros.Append($"<li>{id} - {notificacao.Titulo}</li>");
+                    }
+                }
+            }
+            return erros.ToString();
         }
     }
 }
