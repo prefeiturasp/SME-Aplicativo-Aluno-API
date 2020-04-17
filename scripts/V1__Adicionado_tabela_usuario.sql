23d89adfaa0f7e67be830a4f3398aa8543af847e
Caique Siqueira <latorrecaique@gmail.com>
2020-04-17 20:34:32 -0300

    AB#15084

--- scripts/V1__Adicionado_tabela_usuario.sql
+++ scripts/V1__Adicionado_tabela_usuario.sql
@@ -1,9 +1,9 @@
-CREATE TABLE public.Usuario(
-	id bigint not null generated always as identity,
-	cpf varchar(12) not null,
-	nome varchar(256),
-	ultimoLogin timestamp not null,
-	email(varchar(128),
-	criadoem timestamp,
-	excluido bool
+ CREATE TABLE Usuario (
+  id bigint not null generated always as identity,
+  cpf varchar(12) not null,
+  nome varchar(256),
+  ultimoLogin timestamp not null,
+  email varchar(128),
+  criadoem timestamp,
+  excluido bool
 );
