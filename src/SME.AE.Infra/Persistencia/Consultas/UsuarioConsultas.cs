bcda9e861ca66260c13663889a06a9be228a2c19
Caique Siqueira <latorrecaique@gmail.com>
2020-04-24 15:02:06 -0300

    AB#15270

--- src/SME.AE.Infra/Persistencia/Consultas/UsuarioConsultas.cs
+++ src/SME.AE.Infra/Persistencia/Consultas/UsuarioConsultas.cs
@@ -5,7 +5,7 @@
         internal static string ObterPorCpf = @"
             SELECT Id, Cpf, Nome, UltimoLogin
             FROM Usuario
-            WHERE Usuario.Cpf = @Cpf
-              AND Usuario.excluido = false";
+            WHERE Usuario.Cpf = @Cpf";
+           //  AND Usuario.excluido = false";
     }
 }
