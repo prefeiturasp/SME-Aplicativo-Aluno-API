e0d42f3a880a9bce91eb77660aa599b6b3d8909a
bruno.alevato <bruno.alevato@amcom.com.br>
2020-04-30 04:02:57 -0300

    Commit parcial para atender bug

---
+++ src/SME.AE.Aplicacao/Comandos/Notificacao/EnviarNotificacaoPorGrupo/EnviarNotificacaoPorGrupoCommand.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using SME.AE.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Comandos.Notificacao.EnviarNotificacaoPorGrupo
+{
+    public class EnviarNotificacaoPorGrupoCommand : IRequest<bool>
+    {
+        public SME.AE.Dominio.Entidades.Notificacao Notificacao { set; get; }
+
+        public EnviarNotificacaoPorGrupoCommand(SME.AE.Dominio.Entidades.Notificacao notificacao)
+        {
+            this.Notificacao = notificacao;
+        }
+    }
+
+    public class EnviarNotificacaoPorGrupoCommandHandler : IRequestHandler<EnviarNotificacaoPorGrupoCommand, bool>
+    {
+        private readonly INotificacaoRepository _repository;
+
+        public EnviarNotificacaoPorGrupoCommandHandler(INotificacaoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Handle(EnviarNotificacaoPorGrupoCommand request, CancellationToken cancellationToken)
+        {
+
+            return false;
+        }
+    }
+}
