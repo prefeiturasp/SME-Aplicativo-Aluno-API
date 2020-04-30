4bbe3fa3c5ec2a9831c4fbd2d1dde53bf8704704
alexandreamcom <alexandre.novaes@amcom.com.br>
2020-04-30 16:02:50 -0300

    AB#15805

---
+++ src/SME.AE.Aplicacao/Comum/Modelos/Resposta/NotificacaoResposta.cs
@@ -0,0 +1,22 @@
+﻿using SME.AE.Aplicacao.Comum.Enumeradores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Comum.Modelos.Resposta
+{
+    public class NotificacaoResposta
+    {
+        public long Id { get; set; }
+        public string Mensagem { get; set; }
+        public string Titulo { get; set; }
+        public IEnumerable<Grupo> Grupos { get; set; }
+        public DateTime DataEnvio { get; set; }
+        public DateTime? DataExpiracao { get; set; }
+        public DateTime? CriadoEm { get; set; }
+        public string CriadoPor { get; set; }
+        public DateTime? AlteradoEm { get; set; }
+        public string? AlteradoPor { get; set; }
+    }
+}
