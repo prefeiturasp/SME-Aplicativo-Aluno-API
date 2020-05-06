d8daabf537f8c7bc4899d46f99a7ce1163c34420
alexandreamcom <alexandre.novaes@amcom.com.br>
2020-05-06 14:49:27 -0300

    AB#15835

---
+++ src/SME.AE.Aplicacao/Comum/Interfaces/Repositorios/IUsuarioNotificacaoRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SME.AE.Dominio.Entidades;
+
+namespace SME.AE.Aplicacao.Comum.Interfaces.Repositorios
+{
+    public interface IUsuarioNotificacaoRepositorio
+    {
+        public Task<UsuarioNotificacao> ObterPorId(long id);
+        
+        public Task<UsuarioNotificacao> Criar(Notificacao notificacao);
+        
+        public Task<UsuarioNotificacao> Atualizar(Notificacao notificacao);
+        
+        public Task<bool> Remover(Notificacao notificacao);
+
+    }
+}
---
+++ src/SME.AE.Dominio/Entidades/UsuarioNotificacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SME.AE.Dominio.Entidades
+{
+    [Table("usuario_notificacao_leitura")]
+    public class UsuarioNotificacao
+    {
+        public long Id { get; set; }
+        public int UsuarioId { get; set; }
+        public long CodigoAlunoEol { get; set; }
+        public long NotificacaoId { get; set; }
+        public DateTime? CriadoEm { get; set; }
+        public string CriadoPor { get; set; }
+        public DateTime? AlteradoEm { get; set; }
+        public string AlteradoPor { get; set; }
+    }
+}
