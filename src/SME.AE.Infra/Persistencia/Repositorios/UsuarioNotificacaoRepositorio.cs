5564924d5dc399fed038011aaea1d4f1659ad156
Caique Siqueira <latorrecaique@gmail.com>
2020-05-06 16:00:42 -0300

    AB#15386

--- src/SME.AE.Infra/Persistencia/Repositorios/UsuarioNotificacaoRepositorio.cs
+++ src/SME.AE.Infra/Persistencia/Repositorios/UsuarioNotificacaoRepositorio.cs
@@ -1,4 +1,7 @@
-﻿using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+﻿using Dapper;
+using Sentry;
+using SME.AE.Aplicacao.Comum.Config;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
 using SME.AE.Dominio.Entidades;
 using System;
 using System.Threading.Tasks;
@@ -22,9 +25,28 @@ namespace SME.AE.Infra.Persistencia.Repositorios
             throw new NotImplementedException();
         }
 
-        public Task<bool> Remover(Notificacao notificacao)
+        public async Task<bool> Remover(long notificacaoId)
         {
-            throw new NotImplementedException();
+            bool resultado = false;
+
+            try
+            {
+                await using (var conn = new Npgsql.NpgsqlConnection(ConnectionStrings.Conexao))
+                {
+                    conn.Open();
+
+                    var retorno = await conn.ExecuteAsync(
+                        @"DELETE FROM  where id = @ID", );
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                SentrySdk.CaptureException(ex);
+                return resultado;
+            }
+
+            return resultado;
         }
     }
 }
