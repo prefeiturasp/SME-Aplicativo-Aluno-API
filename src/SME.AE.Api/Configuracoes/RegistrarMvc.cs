4e9e73ba333f20628549b4416943f9efe52649f6
Alexsander Camargo <csalexsander@gmail.com>
2020-06-10 01:26:49 -0300

    Adicionando fluent validation pipeline
    Adicionando fluent validation pipeline
    
    AB#17637

---
+++ src/SME.AE.Api/Configuracoes/RegistrarMvc.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.AE.Api.Configuracoes
+{
+    public static class RegistrarMvc
+    {
+        public static void Registrar(IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+            services.AddMvc(options =>
+            {
+                //options.AllowValidatingTopLevelNodes = false;
+                options.EnableEndpointRouting = true;
+                //options.Filters.Add(new ValidaDtoAttribute());
+                //options.Filters.Add(new FiltroExcecoesAttribute(configuration));
+            })
+                .AddFluentValidation()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+    }
+}
