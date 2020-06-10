8488147500a631531a3245ddf3c744203a3a73c4
Alexsander Camargo <csalexsander@gmail.com>
2020-06-10 18:30:56 -0300

    Ajustes arquitetura
    Ajustes arquitetura
    
    AB#17637

---
+++ src/SME.AE.Api/Configuracoes/RegistraMediatr.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SME.AE.Api.Configuracoes
+{
+    public static class RegistraMediatr
+    {
+        public static void AdicionarMediatr(this IServiceCollection services)
+        {
+            var assembly = AppDomain.CurrentDomain.Load("SME.AE.Aplicacao");
+            services.AddMediatR(assembly);
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidacoesPipeline<,>));
+        }
+    }
+}
