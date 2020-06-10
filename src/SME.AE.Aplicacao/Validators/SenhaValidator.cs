0867e47bd1cd8a7d9e580e6c8faf4b253ef8839f
Alexsander Camargo <csalexsander@gmail.com>
2020-06-10 01:15:25 -0300

    Criar validações de senha
    Criar validações de senha
    
    AB#17637

---
+++ src/SME.AE.Aplicacao/Validators/SenhaValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SME.AE.Aplicacao.Comum.Modelos.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Validators
+{
+    public class SenhaValidator : AbstractValidator<NovaSenhaDto>
+    {
+        public SenhaValidator()
+        {            
+            RuleFor(x => x.NovaSenha).MinimumLength(8);
+            RuleFor(x => x.NovaSenha).MaximumLength(12);
+            RuleFor(x => x.NovaSenha).Must(x => x.Contains(" "));
+            RuleFor(x => x.NovaSenha).Matches(@"(?=.*?[A-Z])(?=.*?[a-z])(?=((?=.*[!@#$\-%&/\\\[\]|*()_=+])|(?=.*?[0-9]+)))");
+        }
+    }
+}
