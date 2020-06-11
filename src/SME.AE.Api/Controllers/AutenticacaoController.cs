d18a6ce191659343fffc9f8c683981ebaccdc6fa
Alexsander Camargo <csalexsander@gmail.com>
2020-06-11 16:05:32 -0300

    Mock
    Mock AB#18082

--- src/SME.AE.Api/Controllers/AutenticacaoController.cs
+++ src/SME.AE.Api/Controllers/AutenticacaoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Castle.DynamicProxy.Generators;
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -35,12 +36,51 @@ namespace SME.AE.Api.Controllers
         {
             try
             {
-                  return Ok(await LogoutUsuarioUseCase.Executar(Mediator, cpf,  dispositivoId));
-              
+                return Ok(await LogoutUsuarioUseCase.Executar(Mediator, cpf, dispositivoId));
+
             }
             catch (Exception ex)
             {
                 return StatusCode(400, ex.Message);
+            }
+        }
+
+        [HttpPost("PrimeiroAcesso")]
+        [AllowAnonymous]
+        public async Task<ActionResult<RespostaApi>> PrimeiroAcesso([FromBody] NovaSenhaDto novaSenhaDto)
+        {
+            var validador = new NovaSenhaDtoValidator();
+
+            var result = await validador.ValidateAsync(novaSenhaDto);
+
+            var respostaAPI = new RespostaApi();
+
+            if (!result.IsValid)
+            {
+                respostaAPI.Erros = result.Errors.Select(x => x.ErrorMessage).ToArray();
+                return StatusCode(400, respostaAPI);
+            }
+
+            respostaAPI.Ok = true;
+
+            return Ok(respostaAPI);
+        }
+
+        public class NovaSenhaDto
+        {
+            public long Id { get; set; }
+            public string NovaSenha { get; set; }
+        }
+
+        public class NovaSenhaDtoValidator : AbstractValidator<NovaSenhaDto>
+        {
+            public NovaSenhaDtoValidator()
+            {
+                RuleFor(x => x.Id).NotNull().GreaterThan(0);
+                RuleFor(x => x.NovaSenha).MinimumLength(8)
+                .MaximumLength(12)
+                .Must(x => !x.Contains(" "))
+                .Matches(@"(?=.*?[A-Z])(?=.*?[a-z])(?=((?=.*[!@#$\-%&/\\\[\]|*()_=+])|(?=.*?[0-9]+)))");
             }
         }
     }
