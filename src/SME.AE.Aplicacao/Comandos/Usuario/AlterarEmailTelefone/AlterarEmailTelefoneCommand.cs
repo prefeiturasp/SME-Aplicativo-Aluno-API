a5e26b63ed6ccb59feb1145290cf8e22a6ec5156
Alexsander Camargo <csalexsander@gmail.com>
2020-06-12 08:51:33 -0300

    Ajustes celular e email
    Ajustes celular e email
    
    AB#17656

---
+++ src/SME.AE.Aplicacao/Comandos/Usuario/AlterarEmailTelefone/AlterarEmailTelefoneCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SME.AE.Aplicacao.Comum.Modelos.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Comandos.Usuario.AlterarEmailTelefone
+{
+    public class AlterarEmailTelefoneCommand : IRequest
+    {
+        public AlterarEmailTelefoneDto alterarEmailTelefoneDto { get; set; }
+    }
+}
---
+++ src/SME.AE.Aplicacao/Comandos/Usuario/AlterarEmailTelefone/AlterarEmailTelefoneCommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Comandos.Usuario.AlterarEmailTelefone
+{
+    class AlterarEmailTelefoneCommandHandler
+    {
+    }
+}
