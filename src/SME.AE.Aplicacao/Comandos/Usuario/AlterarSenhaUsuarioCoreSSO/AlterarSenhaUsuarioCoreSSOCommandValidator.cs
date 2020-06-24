8813c555f8d8daf5a901ff2854a8417c984a5b2b
Alexsander Camargo <csalexsander@gmail.com>
2020-06-24 17:00:00 -0300

    Adicionando metodo alterar senha
    Adicionando metodo alterar senha
    
    AB#18711

---
+++ src/SME.AE.Aplicacao/Comandos/Usuario/AlterarSenhaUsuarioCoreSSO/AlterarSenhaUsuarioCoreSSOCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Comandos.Usuario.AlterarSenhaUsuarioCoreSSO
+{
+    public class AlterarSenhaUsuarioCoreSSOCommandValidator : AbstractValidator<AlterarSenhaUsuarioCoreSSOCommand>
+    {
+        public AlterarSenhaUsuarioCoreSSOCommandValidator()
+        {
+            RuleFor(x => x.UsuarioId).NotNull().NotEmpty();
+            RuleFor(x => x.SenhaCriptograda).NotNull().NotEmpty();
+        }
+    }
+}
