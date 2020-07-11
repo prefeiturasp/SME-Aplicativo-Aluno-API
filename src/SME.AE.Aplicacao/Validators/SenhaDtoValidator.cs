1f0ffbbc3e2fa8500ae83664aa5a7558d96c9801
Alexsander Camargo <csalexsander@gmail.com>
2020-07-11 13:54:47 -0300

    Ajuste endpoint alterar senha
    Ajuste endpoint alterar senha
    
    AB#19660

---
+++ src/SME.AE.Aplicacao/Validators/SenhaDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using SME.AE.Aplicacao.Comum.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Validators
+{
+    public class SenhaDtoValidator : AbstractValidator<SenhaDto>
+    {
+        public SenhaDtoValidator()
+        {
+            RuleFor(x => x.Senha).NotEmpty().WithMessage("È necessario informar a senha");
+            RuleFor(x => x.Senha).ValidarSenha().When(x => !string.IsNullOrWhiteSpace(x.Senha));
+        }
+    }
+}
