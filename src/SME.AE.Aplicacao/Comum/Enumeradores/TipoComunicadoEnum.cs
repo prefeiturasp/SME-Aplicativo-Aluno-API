5a74762740aabeffd02447beee8bcbcac2f8b7ff
Caique Siqueira <latorrecaique@gmail.com>
2020-07-22 15:20:26 -0300

    AB#20361 Mock Retorno de categoria notificacao

---
+++ src/SME.AE.Aplicacao/Comum/Enumeradores/TipoComunicadoEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Comum.Enumeradores
+{
+   public enum TipoComunicadoEnum
+    {
+        SME = 1,
+        DRE = 2, 
+        UE = 3, 
+        TURMA = 4 ,
+        UE_Modalidade = 5 , 
+        DRE_Modalidade =6 , 
+        Aluno_Especificado = 7
+    }
+}
--- src/SME.AE.Aplicacao/Comum/Modelos/NotificacaoSgpDto.cs
+++ src/SME.AE.Aplicacao/Comum/Modelos/NotificacaoSgpDto.cs
@@ -1,15 +1,38 @@
-﻿using System;
+﻿using SME.AE.Aplicacao.Comum.Enumeradores;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SME.AE.Aplicacao.Comum.Modelos
 {
     public class NotificacaoSgpDto : ModeloBase
+    //{
+    //    public string Mensagem { get; set; }
+    //    public string Titulo { get; set; }
+    //    public string Grupo { get; set; }
+    //    public DateTime DataEnvio { get; set; }
+    //    public DateTime? DataExpiracao { get; set; }
+
     {
-        public string Mensagem { get; set; }
-        public string Titulo { get; set; }
-        public string Grupo { get; set; }
+       
+        public string CriadoRF { get; set; }
         public DateTime DataEnvio { get; set; }
         public DateTime? DataExpiracao { get; set; }
+        public string Grupo { get; set; }
+        public IEnumerable<string> Alunos { get; set; }
+        public long Id { get; set; }
+        public string Mensagem { get; set; }
+        public string Titulo { get; set; }
+        public int AnoLetivo { get; set; }
+        public string CodigoDre { get; set; }
+        public string CodigoUe { get; set; }
+        public string Turma { get; set; }
+       // public Modalidade? Modalidade { get; set; }
+        public int? Semestre { get; set; }
+        public TipoComunicadoEnum TipoComunicado { get; set; }
     }
+
+
+
+
 }
