b1678945b907241b384cbde555107e5b7b853317
Caique Siqueira <latorrecaique@gmail.com>
2020-07-27 01:04:05 -0300

    AB#20567- Ajustes

--- src/SME.AE.Aplicacao/Comum/Modelos/NotificacaoSgpDto.cs
+++ src/SME.AE.Aplicacao/Comum/Modelos/NotificacaoSgpDto.cs
@@ -7,32 +7,18 @@ namespace SME.AE.Aplicacao.Comum.Modelos
 {
     public class NotificacaoSgpDto : ModeloBase
     {
-        //public string Mensagem { get; set; }
-        //public string Titulo { get; set; }
-        //public string Grupo { get; set; }
-        //public DateTime DataEnvio { get; set; }
-        //public DateTime? DataExpiracao { get; set; }
-
-        public DateTime? AlteradoEm { get; set; }
-        public string AlteradoPor { get; set; }
         public string AlteradoRF { get; set; }
-        public DateTime CriadoEm { get; set; }
-        public string CriadoPor { get; set; }
         public string CriadoRF { get; set; }
         public DateTime DataEnvio { get; set; }
         public DateTime? DataExpiracao { get; set; }
         public string Grupo { get; set; }
         public IEnumerable<string> Alunos { get; set; }
-        public long Id { get; set; }
         public string Mensagem { get; set; }
         public string Titulo { get; set; }
         public int AnoLetivo { get; set; }
         public string CodigoDre { get; set; }
         public string CodigoUe { get; set; }
         public IEnumerable<string> Turma { get; set; }
-        //public int? Semestre { get; set; }
         public TipoComunicado TipoComunicado { get; set; }
-
-
     }
 }
