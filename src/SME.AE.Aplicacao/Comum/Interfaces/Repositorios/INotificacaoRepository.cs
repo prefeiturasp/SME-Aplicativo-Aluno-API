b911f07de5df07eb32726385dc9731ff345baeab
Alexsander Camargo <csalexsander@gmail.com>
2020-08-05 12:16:28 -0300

    Correção confirmação de leitura
    Correção confirmação de leitura
    
    AB#21334

--- src/SME.AE.Aplicacao/Comum/Interfaces/Repositorios/INotificacaoRepository.cs
+++ src/SME.AE.Aplicacao/Comum/Interfaces/Repositorios/INotificacaoRepository.cs
@@ -7,11 +7,9 @@ using SME.AE.Dominio.Entidades;
 
 namespace SME.AE.Aplicacao.Comum.Interfaces.Repositorios
 {
-    public interface INotificacaoRepository
+    public interface INotificacaoRepository : IBaseRepositorio<Notificacao>
     {
         public Task<IEnumerable<NotificacaoPorUsuario>> ObterPorGrupoUsuario(string grupo, string cpf);
-
-        public Task<Notificacao> ObterPorId(long id);
 
         public Task Criar(Notificacao notificacao);
 
@@ -22,6 +20,8 @@ namespace SME.AE.Aplicacao.Comum.Interfaces.Repositorios
         public Task<IDictionary<string, object>> ObterGruposDoResponsavel(string cpf, string grupos, string nomeGrupos);
 
         public Task<IEnumerable<string>> ObterResponsaveisPorGrupo(string where);
+
+        Task<IEnumerable<NotificacaoTurma>> ObterTurmasPorNotificacao(long id);
 
         public Task InserirNotificacaoAluno(NotificacaoAluno notificacaoAluno);
 
