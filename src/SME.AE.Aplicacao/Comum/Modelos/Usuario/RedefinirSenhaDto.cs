77e36aea26a943232148a596174bda3c9c46cdb0
Alexsander Camargo <csalexsander@gmail.com>
2020-08-05 16:59:12 -0300

    Mock
    Mock
    
    AB#21382

---
+++ src/SME.AE.Aplicacao/Comum/Modelos/Usuario/RedefinirSenhaDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Comum.Modelos.Usuario
+{
+    public class RedefinirSenhaDto
+    {
+        public string Token { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ src/SME.AE.Aplicacao/Validators/RedefinirSenhaValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using SME.AE.Aplicacao.Comum.Modelos.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Validators
+{
+    public class RedefinirSenhaValidator : AbstractValidator<RedefinirSenhaDto>
+    {
+        public RedefinirSenhaValidator()
+        {
+            RuleFor(x => x.Senha).NotEmpty().WithMessage("É Obrigátorio informar a senha");
+
+            RuleFor(x => x.Senha).ValidarSenha().When(x => !string.IsNullOrWhiteSpace(x.Senha));
+
+            RuleFor(x => x.Token).NotEmpty().WithMessage("É Obrigátorio informar o token");
+
+            RuleFor(x => x.Token).Length(8).WithMessage("O Tamanho do token esta invalido").When(x => !string.IsNullOrWhiteSpace(x.Token));
+        }
+    }
+}
