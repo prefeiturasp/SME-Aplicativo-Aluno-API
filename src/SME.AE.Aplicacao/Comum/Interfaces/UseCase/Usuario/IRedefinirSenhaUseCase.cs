609e8d01e204e12aa64c071049d864314e3b4611
Alexsander Camargo <csalexsander@gmail.com>
2020-08-07 12:29:53 -0300

    Novo endpoint de redefinição de senha
    Novo endpoint de redefinição de senha
    
    AB#20951

---
+++ src/SME.AE.Aplicacao/Comum/Interfaces/UseCase/Usuario/IRedefinirSenhaUseCase.cs
@@ -0,0 +1,14 @@
+﻿using SME.AE.Aplicacao.Comum.Modelos;
+using SME.AE.Aplicacao.Comum.Modelos.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Comum.Interfaces.UseCase.Usuario
+{
+    public interface IRedefinirSenhaUseCase
+    {
+        Task<RespostaApi> Executar(RedefinirSenhaDto dto);
+    }
+}
--- src/SME.AE.Aplicacao/Consultas/ObterUsuarioPorTokenRedefinicao/ObterUsuarioPorTokenRedefinicaoQueryValidator.cs
+++ src/SME.AE.Aplicacao/Consultas/ObterUsuarioPorTokenRedefinicao/ObterUsuarioPorTokenRedefinicaoQueryValidator.cs
@@ -3,11 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SME.AE.Aplicacao.Consultas.ObterUsuarioPorTokenAutenticacao
+namespace SME.AE.Aplicacao.Consultas.ObterUsuarioPorTokenRedefinicao
 {
-    public class ObterUsuarioPorTokenAutenticacaoCommandValidator : AbstractValidator<ObterUsuarioPorTokenAutenticacaoCommand>
+    public class ObterUsuarioPorTokenRedefinicaoQueryValidator : AbstractValidator<ObterUsuarioPorTokenRedefinicaoQuery>
     {
-        public ObterUsuarioPorTokenAutenticacaoCommandValidator()
+        public ObterUsuarioPorTokenRedefinicaoQueryValidator()
         {
             RuleFor(x => x.Token).NotEmpty().WithMessage("O Token é Obrigátorio");
             RuleFor(x => x.Token).Length(8).When(x => !string.IsNullOrWhiteSpace(x.Token)).WithMessage("O Token possui tamanho inválido");
