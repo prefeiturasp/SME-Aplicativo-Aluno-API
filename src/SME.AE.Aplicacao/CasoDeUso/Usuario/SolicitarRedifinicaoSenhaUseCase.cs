d8d0bd328c85b14307f0ab81c7ec1afc64c9ff3f
Alexsander Camargo <csalexsander@gmail.com>
2020-08-07 21:25:46 -0300

    Update SolicitarRedifinicaoSenhaUseCase.cs

--- src/SME.AE.Aplicacao/CasoDeUso/Usuario/SolicitarRedifinicaoSenhaUseCase.cs
+++ src/SME.AE.Aplicacao/CasoDeUso/Usuario/SolicitarRedifinicaoSenhaUseCase.cs
@@ -52,7 +52,7 @@ namespace SME.AE.Aplicacao.CasoDeUso
         {
             var caminhot = Directory.GetCurrentDirectory();
 
-            SentrySdk.AddBreadcrumb($"caminho diretorio = {caminhot}");
+            SentrySdk.CaptureMessage($"caminho diretorio = {caminhot}");
 
             string caminho = $"{Directory.GetCurrentDirectory()}/wwwroot/ModelosEmail/RecuperacaoSenha.html";
             var textoArquivo = await File.ReadAllTextAsync(caminho);
