def2bb9b5ce5e270959502f1d35f44b396d0fcc4
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-08-21 10:56:57 -0300

    Implementação de cache com testes de gravação e leitura

---
+++ src/SME.AE.Aplicacao/Consultas/Cache/ObterCacheQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace SME.AE.Aplicacao.Consultas.ObterUsuario
+{
+    public class ObterCacheQuery : IRequest<string>
+    {
+        public ObterCacheQuery(string cpf)
+        {
+            Cpf = cpf;
+        }
+
+        public string Cpf { get; set; }
+    }
+}
