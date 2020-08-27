f600b51417c44be0acf8de8f4fb3b556c83a0577
Rodrigo Galante <rodrigo.galante@globo.com>
2020-08-27 16:26:00 -0300

    Ajustes pós cherry-pick

--- src/SME.AE.Aplicacao/Comum/Interfaces/Repositorios/IAlunoRepositorio.cs
+++ src/SME.AE.Aplicacao/Comum/Interfaces/Repositorios/IAlunoRepositorio.cs
@@ -1,15 +1,11 @@
 ﻿using SME.AE.Aplicacao.Comum.Modelos.Resposta;
-using SME.AE.Dominio.Entidades;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SME.AE.Aplicacao.Comum.Interfaces.Repositorios
 {
-  public  interface IAlunoRepositorio
+    public  interface IAlunoRepositorio
     {
         Task<List<AlunoRespostaEol>> ObterDadosAlunos(string cpf);
-        Task<List<AlunoRespostaEol>> ObterDadosAlunosCache(string cpf);
     }
 }
