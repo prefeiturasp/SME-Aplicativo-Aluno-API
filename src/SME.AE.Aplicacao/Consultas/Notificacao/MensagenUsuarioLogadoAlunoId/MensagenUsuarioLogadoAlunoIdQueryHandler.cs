5e5b16140d4961a2a1304471c1a69588b6fafb21
Ivano Proieti Mucci <ivano.mucci@amcom.com.br>
2020-09-03 10:13:11 -0300

    Criacao endpoint para mensagen por Id

---
+++ src/SME.AE.Aplicacao/Consultas/Notificacao/MensagenUsuarioLogadoAlunoId/MensagenUsuarioLogadoAlunoIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Org.BouncyCastle.Crypto;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Consultas.Notificacao.ListarNotificacaoAluno
+{
+    public class MensagenUsuarioLogadoAlunoIdQueryHandler : IRequestHandler<MensagenUsuarioLogadoAlunoIdQuery, NotificacaoResposta>
+    {
+        private readonly INotificacaoRepository notificacaoRepository;
+        private readonly IGrupoComunicadoRepository grupoComunicadoRepository;
+
+        public MensagenUsuarioLogadoAlunoIdQueryHandler(INotificacaoRepository notificacaoRepository, IGrupoComunicadoRepository grupoComunicadoRepository)
+        {
+            this.notificacaoRepository = notificacaoRepository ?? throw new ArgumentNullException(nameof(notificacaoRepository));
+            this.grupoComunicadoRepository = grupoComunicadoRepository ?? throw new ArgumentNullException(nameof(grupoComunicadoRepository));
+        }
+
+        public async Task<NotificacaoResposta> Handle(MensagenUsuarioLogadoAlunoIdQuery request, CancellationToken cancellationToken)
+        {
+            return await notificacaoRepository.NotificacaoPorId(request.Id);
+        }
+    }
+}
