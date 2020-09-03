2f6162e3e8fc486d91c0f504b31a2433f3bfc133
Rodrigo Galante <rodrigo.galante@globo.com>
2020-09-03 11:18:33 -0300

    Implementação de busca de usuário por CPF

---
+++ src/SME.AE.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.AE.Aplicacao.Comum.Interfaces.UseCase;
+using SME.AE.Aplicacao.Comum.Modelos;
+using System.Threading.Tasks;
+
+namespace SME.AE.Api.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        [HttpGet("{cpf}")]
+        [AllowAnonymous]
+        public async Task<ObjectResult> ObterUsuariosPorCpf(string cpf, [FromServices] IObterUsuarioUseCase obterUsuarioUseCase)
+        {
+            return Ok(await obterUsuarioUseCase.Executar(cpf));
+        }
+
+    }
+}
---
+++ src/SME.AE.Aplicacao/Comum/Interfaces/UseCase/Usuario/IObterUsuarioUseCase.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Comum.Interfaces.UseCase
+{
+    public interface IObterUsuarioUseCase
+    {
+        Task<UsuarioDto> Executar(string cpf);
+    }
+}
