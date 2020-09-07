b8a743616131e5cf27c42bef8fe5378483255c10
Rodrigo Galante <rodrigo.galante@globo.com>
2020-09-07 12:46:19 -0300

    Ajustes de formatação de cpf, Handler de obter usuario por cpf

---
+++ src/SME.AE.Aplicacao/CasoDeUso/Usuario/ObterUsuarioUseCase.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using SME.AE.Aplicacao.Comum.Interfaces.UseCase;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using SME.AE.Aplicacao.Consultas.ObterUsuario;
+using SME.AE.Comum.Excecoes;
+using SME.AE.Comum.Utilitarios;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.CasoDeUso
+{
+    public class ObterUsuarioUseCase : IObterUsuarioUseCase
+    {
+        private readonly IMediator mediator;
+
+        public ObterUsuarioUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        public async Task<UsuarioDto> Executar(string cpf)
+        {
+            var usuario = await mediator.Send(new ObterUsuarioPorCpfQuery(cpf));
+            if (usuario == null)
+                throw new NegocioException($"O usuário {Formatacao.FormatarCpf(cpf)} deverá informar a data de nascimento de um dos estudantes que é responsável no campo de senha!");
+
+
+
+            return new UsuarioDto(usuario.Cpf, usuario.Nome);
+        }
+    }
+}
---
+++ src/SME.AE.Aplicacao/Consultas/ObterUsuarioPorCpf/ObterUsuarioPorCpfQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using SME.AE.Dominio.Entidades;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Consultas.ObterUsuario
+{
+    public class ObterUsuarioPorCpfQueryHandler : IRequestHandler<ObterUsuarioPorCpfQuery, Usuario>
+    {
+        private readonly IUsuarioRepository usuarioRepository;
+
+        public ObterUsuarioPorCpfQueryHandler(IUsuarioRepository usuarioRepository)
+        {
+            this.usuarioRepository = usuarioRepository ?? throw new System.ArgumentNullException(nameof(usuarioRepository));
+        }
+
+        public async Task<Usuario> Handle(ObterUsuarioPorCpfQuery request, CancellationToken cancellationToken)
+        {
+            return await usuarioRepository.ObterPorCpf(request.Cpf);
+        }
+    }
+}
