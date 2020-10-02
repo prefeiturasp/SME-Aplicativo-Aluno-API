af8f5c2269d38ce450664ccdf90414182b3bf548
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-10-02 09:05:48 -0300

    Adicionada tabela de eventos e caso de uso pra carga a partir do sgp

---
+++ src/SME.AE.Infra/Persistencia/Repositorios/EventoRepositorio.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using Npgsql;
+using SME.AE.Aplicacao.Comum.Config;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using SME.AE.Aplicacao.Comum.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.AE.Infra.Persistencia.Repositorios
+{
+    public class EventoRepositorio : IEventoRepositorio
+    {
+        private NpgsqlConnection CriaConexao() => new NpgsqlConnection(ConnectionStrings.Conexao);
+
+        public async Task<DateTime?> ObterUltimaAlteracao()
+        {
+            var sql = @"select max(ultima_alteracao_sgp) from evento";
+            using var conn = CriaConexao();
+            await conn.OpenAsync();
+            var ultimaAlteracao = await conn.QueryFirstOrDefaultAsync<DateTime?>(sql);
+            await conn.CloseAsync();
+            return ultimaAlteracao;
+        }
+
+        public async Task Salvar(EventoDto evento)
+        {
+            var sql = @"
+                insert into evento
+	                (evento_id, nome, descricao, data_inicio, data_fim, dre_id, ue_id, tipo_evento, turma_id, ano_letivo, modalidade, ultima_alteracao_sgp)
+                values
+	                (@evento_id, @nome, @descricao, @data_inicio, @data_fim, @dre_id, @ue_id, @tipo_evento, @turma_id, @ano_letivo, @modalidade, @ultima_alteracao_sgp)
+                on conflict 
+	                (evento_id) 
+                do update set 
+	                nome = excluded.nome, 
+	                descricao = excluded.descricao, 
+	                data_inicio = excluded.data_inicio, 
+	                data_fim = excluded.data_fim, 
+	                dre_id = excluded.dre_id, 
+	                ue_id = excluded.ue_id, 
+	                tipo_evento = excluded.tipo_evento, 
+	                turma_id = excluded.turma_id, 
+	                ano_letivo = excluded.ano_letivo, 
+	                modalidade = excluded.modalidade, 
+	                ultima_alteracao_sgp = excluded.ultima_alteracao_sgp
+            ";
+            using var conn = CriaConexao();
+            await conn.OpenAsync();
+            await conn.ExecuteAsync(sql, evento);
+            await conn.CloseAsync();
+            return;
+        }
+    }
+}
---
+++ src/SME.AE.Infra/Persistencia/Repositorios/EventoSgpRepositorio.cs
@@ -0,0 +1,75 @@
+﻿using Dapper;
+using Npgsql;
+using SME.AE.Aplicacao.Comum.Config;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using SME.AE.Aplicacao.Comum.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.AE.Infra.Persistencia.Repositorios
+{
+    public class EventoSgpRepositorio : IEventoSgpRepositorio
+    {
+		private NpgsqlConnection CriaConexao() => new NpgsqlConnection(ConnectionStrings.ConexaoSgp);
+
+		public async Task<IEnumerable<EventoSgpDto>> ListaEventoPorDataAlteracao(DateTime ultimaDataAlteracao)
+        {
+			var sql =
+				@"
+					select * from 
+					(
+						(select 
+							aa.id, aa.nome_avaliacao nome, aa.descricao_avaliacao descricao, aa.data_avaliacao data_inicio, aa.data_avaliacao data_fim,
+							aa.dre_id, aa.ue_id, 
+							0::int tipo_evento_id,
+							aa.turma_id,
+							t.ano_letivo,
+							t.modalidade_codigo modalidade_turma,
+							null::int modalidade_calendario,
+							greatest(aa.criado_em, aa.alterado_em) alterado_em
+						from atividade_avaliativa aa 
+						inner join turma t on t.turma_id = aa.turma_id 
+						where
+							(aa.migrado isnull or aa.migrado = false) and
+							(aa.excluido isnull or aa.excluido = false)
+						)
+						union 
+						(select
+							e.id, e.nome, e.descricao, e.data_inicio, e.data_fim,
+							e.dre_id, e.ue_id,
+							e.tipo_evento_id,
+							null::varchar turma_id,
+							tc.ano_letivo,
+							null::int modalidade_turma,
+							tc.modalidade modalidade_calendario,
+							greatest(
+								e.criado_em, e.alterado_em,
+								tc.criado_em, tc.alterado_em,
+								wan.criado_em, wan.alterado_em,
+								n2.criado_em, n2.alterado_em) alterado_em
+						from evento e
+						inner join evento_tipo et on et.id = e.tipo_evento_id
+						inner join tipo_calendario tc on tc.id = e.tipo_calendario_id 
+						left join wf_aprovacao_nivel wan on wan.wf_aprovacao_id = e.wf_aprovacao_id 
+						left join wf_aprovacao_nivel_notificacao wann on wann.wf_aprovacao_nivel_id = wan.id 
+						left join notificacao n2 on n2.id = wann.notificacao_id 
+						where
+							(e.status = 1) and
+							(e.migrado isnull or e.migrado = false) and
+							(e.excluido isnull or e.excluido = false) and
+							(et.evento_escolaaqui)
+						)
+					) as eae
+					where alterado_em > @alterado_em 
+				";
+
+			using var conn = CriaConexao();
+			await conn.OpenAsync();
+			var eventosSgp = await conn.QueryAsync<EventoSgpDto>(sql, new { alterado_em = ultimaDataAlteracao });
+			await conn.CloseAsync();
+			return eventosSgp;
+		}
+	}
+}
