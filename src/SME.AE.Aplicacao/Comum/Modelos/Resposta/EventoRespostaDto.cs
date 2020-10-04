ba81a801090e61d220b7e5aa2eee489a630c6b36
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-10-03 21:16:28 -0300

    endpoint evento por aluno/mes

---
+++ src/SME.AE.Aplicacao/Comum/Modelos/Resposta/EventoRespostaDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Comum.Modelos.Resposta
+{
+    public class EventoRespostaDto
+    {
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public DateTime data_inicio { get; set; }
+        public DateTime data_fim { get; set; }
+        public int tipo_evento { get; set; }
+        public int ano_letivo { get; set; }
+    }
+}
---
+++ src/SME.AE.Aplicacao/Consultas/ObterEventosAlunoPorMes/ObterEventosAlunoPorMesQueryHandler.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Consultas
+{
+    class ObterEventosAlunoPorMesQueryHandler : IRequestHandler<ObterEventosAlunoPorMesQuery, IEnumerable<EventoRespostaDto>>
+    {
+        private readonly IEventoRepositorio eventoRepositorio;
+        private readonly IAlunoRepositorio alunoRepositorio;
+
+        public ObterEventosAlunoPorMesQueryHandler(IEventoRepositorio eventoRepositorio, IAlunoRepositorio alunoRepositorio)
+        {
+            this.eventoRepositorio = eventoRepositorio ?? throw new ArgumentNullException(nameof(eventoRepositorio));
+            this.alunoRepositorio = alunoRepositorio ?? throw new ArgumentNullException(nameof(alunoRepositorio));
+        }
+        public async Task<IEnumerable<EventoRespostaDto>> Handle(ObterEventosAlunoPorMesQuery request, CancellationToken cancellationToken)
+        {
+            var aluno = (await alunoRepositorio.ObterDadosAlunos(request.Cpf)).Where(a => a.CodigoEol == request.CodigoAluno).FirstOrDefault();
+
+            var modalidade = 0;
+
+            switch (aluno.CodigoModalidadeTurma)
+            {
+                case 1:
+                    modalidade = 3;
+                    break;
+                case 3:
+                    modalidade = 2;
+                    break;
+                case 5:
+                case 6:
+                    modalidade = 1;
+                    break;
+                default:
+                    modalidade = aluno.CodigoModalidadeTurma;
+                    break;
+            }
+            var eventos = await eventoRepositorio.ObterPorDreUeTurmaMes(aluno.CodigoDre, aluno.CodigoEscola, aluno.CodigoTurma.ToString(), modalidade, request.MesAno);
+            var eventosResposta = eventos.Select(
+                    e => new EventoRespostaDto
+                    {
+                        nome = e.nome,
+                        descricao = e.descricao,
+                        data_inicio = e.data_inicio,
+                        data_fim = e.data_fim,
+                        ano_letivo = e.ano_letivo,
+                        tipo_evento = e.tipo_evento
+                    }
+                ).Distinct();
+
+            return eventosResposta;
+        }
+    }
+}
