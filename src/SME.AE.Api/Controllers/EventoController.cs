04134f590552405ab09f6d80f53347bd08e5e14e
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-10-03 21:16:28 -0300

    endpoint evento por aluno/mes

---
+++ src/SME.AE.Api/Controllers/EventoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.AE.Aplicacao.Comum.Interfaces.UseCase;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.AE.Api.Controllers
+{
+    public class EventoController: ApiController
+    {
+        [HttpGet("AlunoLogado/{ano}/{mes}/{codigoAluno}")]
+        [Authorize]
+        public async Task<ActionResult<EventoRespostaDto>> ObterDoAlunoLogado(long codigoAluno, int mes, int ano, [FromServices] IObterEventosAlunoPorMesUseCase obterDoAlunoLogado)
+        {
+            return Ok(await obterDoAlunoLogado.Executar(User.Identity.Name, codigoAluno, mes, ano));
+        }
+    }
+}
