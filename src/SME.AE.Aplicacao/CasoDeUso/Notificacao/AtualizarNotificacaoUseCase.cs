b40efcb30c3171c1639521d8c808c5220cb419f1
Rodrigo Galante <rodrigo.galante@globo.com>
2020-10-09 22:23:13 -0300

    Correção e refactory de fluxo de atualização de notificação

--- src/SME.AE.Aplicacao/CasoDeUso/Notificacao/AtualizarNotificacaoUseCase.cs
+++ src/SME.AE.Aplicacao/CasoDeUso/Notificacao/AtualizarNotificacaoUseCase.cs
@@ -1,33 +1,30 @@
-﻿using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using MediatR;
 using SME.AE.Aplicacao.Comandos.Notificacao.Atualizar;
 using SME.AE.Aplicacao.Comum.Interfaces.UseCase;
 using SME.AE.Aplicacao.Comum.Modelos;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta;
 using SME.AE.Comum.Excecoes;
-using SME.AE.Dominio.Entidades;
+using System.Threading.Tasks;
 
 namespace SME.AE.Aplicacao
 {
     public class AtualizarNotificacaoUseCase : IAtualizarNotificacaoUseCase
     {
         private readonly IMediator mediator;
-        private readonly IMapper mapper;
 
         public AtualizarNotificacaoUseCase(IMediator mediator, IMapper mapper)
         {
             this.mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
-            this.mapper = mapper ?? throw new System.ArgumentNullException(nameof(mapper));
         }
 
-        public async Task<NotificacaoSgpDto> Executar(NotificacaoSgpDto notificacao)
+        public async Task<AtualizacaoNotificacaoResposta> Executar(NotificacaoSgpDto notificacao)
         {
-            var resultado = await mediator.Send(new AtualizarNotificacaoCommand(mapper.Map<Notificacao>(notificacao)));
-            
+            var resultado = await mediator.Send(new AtualizarNotificacaoCommand(notificacao.Id, notificacao.Titulo, notificacao.Mensagem, notificacao.DataExpiracao, notificacao.AlteradoPor));
             if (resultado == null)
                 throw new NegocioException("Não foi possível atualizar o comunicado na base do Escola Aqui!");
 
-            return mapper.Map<NotificacaoSgpDto>(resultado);
+            return resultado;
         }
     }
 }
--- src/SME.AE.Aplicacao/Comandos/Notificacao/Atualizar/AtualizarNotificacaoCommand.cs
+++ src/SME.AE.Aplicacao/Comandos/Notificacao/Atualizar/AtualizarNotificacaoCommand.cs
@@ -1,34 +1,28 @@
 ﻿using MediatR;
-using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
-using System.Threading;
-using System.Threading.Tasks;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using System;
 
 namespace SME.AE.Aplicacao.Comandos.Notificacao.Atualizar
 {
-    public class AtualizarNotificacaoCommand : IRequest<Dominio.Entidades.Notificacao>
+    public class AtualizarNotificacaoCommand : IRequest<AtualizacaoNotificacaoResposta>
     {
-        public Dominio.Entidades.Notificacao Notificacao { get; set; }
+        public long Id { get; set; }
 
-        public AtualizarNotificacaoCommand(Dominio.Entidades.Notificacao notificacao)
+        public string Titulo { get; set; }
+
+        public string Mensagem { get; set; }
+
+        public DateTime DataExpiracao { get; set; }
+
+        public string AlteradoPor { get; set; }
+
+        public AtualizarNotificacaoCommand(long id, string titulo, string mensagem, DateTime dataExpiracao, string alteradoPor)
         {
-            Notificacao = notificacao;
-        }
-    }
-
-    public class AtualizarNotificacaoCommandHandler : IRequestHandler<AtualizarNotificacaoCommand,
-        Dominio.Entidades.Notificacao>
-    {
-        private readonly INotificacaoRepository _repository;
-
-        public AtualizarNotificacaoCommandHandler(INotificacaoRepository repository)
-        {
-            _repository = repository;
-        }
-
-        public async Task<Dominio.Entidades.Notificacao> Handle
-            (AtualizarNotificacaoCommand request, CancellationToken cancellationToken)
-        {
-            return await _repository.Atualizar(request.Notificacao); ;
+            Id = id;
+            Titulo = titulo;
+            Mensagem = mensagem;
+            DataExpiracao = dataExpiracao;
+            AlteradoPor = alteradoPor;
         }
     }
 }
