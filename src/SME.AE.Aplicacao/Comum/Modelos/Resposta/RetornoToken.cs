1d3bf7f1d5bee441accff3856dfddb9c2a2e6bb1
Rodrigo Galante <rodrigo.galante@globo.com>
2020-10-13 19:14:54 -0300

    Implementação de retorno do token para AlterarEmailCelular

---
+++ src/SME.AE.Aplicacao/Comum/Modelos/Resposta/RetornoToken.cs
@@ -0,0 +1,12 @@
+﻿namespace SME.AE.Aplicacao.Comum.Modelos.Resposta
+{
+    public class RetornoToken
+    {
+        public string Token { get; set; }
+
+        public RetornoToken(string token)
+        {
+            Token = token;
+        }
+    }
+}
