359e86ff242380528cbfbd2a3ab758a04e8531e1
Rodrigo Galante <rodrigo.galante@globo.com>
2020-10-28 18:47:07 -0300

    Endpoint de exibição de dados de Frequencia do Aluno

---
+++ src/SME.AE.Aplicacao/Comum/Interfaces/UseCase/Frequencia/IObterFrequenciaAlunoUseCase.cs
@@ -0,0 +1,12 @@
+﻿using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Comum.Interfaces.UseCase
+{
+    public interface IObterFrequenciaAlunoUseCase
+    {
+        Task<IEnumerable<FrequenciaAlunoResposta>> Executar(string codigoUe, long codigoTurma, string codigoAluno);
+    }
+}
+
