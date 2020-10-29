30ba2ff250af1177e73ff492d47a49209772d1a8
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-10-29 05:04:55 -0300

    Caso de uso de transferencia de frequencia

--- src/SME.AE.Aplicacao/CasoDeUso/AgendadoWorkerService/TransferirFrequenciaSgpCasoDeUso.cs
+++ src/SME.AE.Aplicacao/CasoDeUso/AgendadoWorkerService/TransferirFrequenciaSgpCasoDeUso.cs
@@ -10,13 +10,19 @@ namespace SME.AE.Aplicacao.CasoDeUso
 {
     public class TransferirFrequenciaSgpCasoDeUso
     {
-        public TransferirFrequenciaSgpCasoDeUso()
+        private readonly IFrequenciaAlunoRepositorio frequenciaAlunoRepositorio;
+        private readonly IFrequenciaAlunoSgpRepositorio frequenciaAlunoSgpRepositorio;
+
+        public TransferirFrequenciaSgpCasoDeUso(IFrequenciaAlunoRepositorio frequenciaAlunoRepositorio, IFrequenciaAlunoSgpRepositorio frequenciaAlunoSgpRepositorio)
         {
+            this.frequenciaAlunoRepositorio = frequenciaAlunoRepositorio ?? throw new ArgumentNullException(nameof(frequenciaAlunoRepositorio));
+            this.frequenciaAlunoSgpRepositorio = frequenciaAlunoSgpRepositorio ?? throw new ArgumentNullException(nameof(frequenciaAlunoSgpRepositorio));
         }
 
         public async Task ExecutarAsync()
         {
-            await Task.CompletedTask;
+            IEnumerable<FrequenciaAlunoSgpDto> frequenciaAlunosSgp = await frequenciaAlunoSgpRepositorio.ObterFrequenciaAlunoSgp();
+            await frequenciaAlunoRepositorio.SalvarFrequenciaAlunosBatch(frequenciaAlunosSgp);
         }
     }
 }
