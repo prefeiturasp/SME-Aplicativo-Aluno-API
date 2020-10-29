f40356838ffe070cbae58281d76f86a57810403f
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-10-28 21:25:29 -0300

    Esqueleto worker e caso de uso inicial

---
+++ src/SME.AE.Aplicacao/CasoDeUso/AgendadoWorkerService/TransferirFrequenciaSgpCasoDeUso.cs
@@ -0,0 +1,22 @@
+﻿using SME.AE.Aplicacao.Comum.Enumeradores;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using SME.AE.Aplicacao.Comum.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.CasoDeUso
+{
+    public class TransferirFrequenciaSgpCasoDeUso
+    {
+        public TransferirFrequenciaSgpCasoDeUso()
+        {
+        }
+
+        public async Task ExecutarAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
