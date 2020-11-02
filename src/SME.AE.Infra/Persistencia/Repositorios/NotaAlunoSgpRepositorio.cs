31a216ab9e092bb0737f819946d426c5266c2894
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-11-02 19:19:21 -0300

    Melhora na busca de notas do sgp

--- src/SME.AE.Infra/Persistencia/Repositorios/NotaAlunoSgpRepositorio.cs
+++ src/SME.AE.Infra/Persistencia/Repositorios/NotaAlunoSgpRepositorio.cs
@@ -25,32 +25,49 @@ namespace SME.AE.Infra.Persistencia.Repositorios
 
                 var notaAlunosSgp = await conexao
                     .QueryAsync<NotaAlunoSgpDto>(
-                        @"
-                        select distinct 
-	                        t.ano_letivo AnoLetivo,
-	                        aa.ue_id    CodigoUe,
-	                        aa.turma_id CodigoTurma,
-	                        pe.bimestre,
-	                        nc.aluno_id CodigoAluno,
-	                        cc.id CodigoComponenteCurricular,
-	                        coalesce(cc.descricao_sgp, cc.descricao) ComponenteCurricular,
-	                        coalesce (
-		                        coalesce(ccn.nota::varchar, cv2.valor),
-		                        coalesce(nc.nota::varchar, cv.valor)
-	                        ) Nota
-                        from 
-	                        fechamento_turma ft
-                        inner join periodo_escolar pe on pe.id = ft.periodo_escolar_id 
-                        inner join turma t on t.id = ft.turma_id
-                        inner join atividade_avaliativa aa on aa.turma_id = t.turma_id 
-                        inner join notas_conceito nc on nc.atividade_avaliativa = aa.id 
-                        left join componente_curricular cc on cc.id::varchar = nc.disciplina_id 
-                        left join conceito_valores cv on cv.id = nc.conceito
-                        left join conselho_classe_aluno cca on cca.aluno_codigo = nc.aluno_id 
-                        left join conselho_classe_nota ccn on ccn.conselho_classe_aluno_id = cca.id 
-                        left join conceito_valores cv2 on cv2.id = ccn.conceito_id 
-                        left join conselho_classe cc2 on cc2.id = cca.conselho_classe_id 
-                        where t.ano_letivo >= 2020	
+						@"
+						select 
+						distinct 
+							t.ano_letivo 		AnoLetivo,
+							ue.ue_id   			CodigoUe,
+							t.turma_id 			CodigoTurma,
+							coalesce(
+								pe.bimestre,
+								0)				Bimestre,
+							fa.aluno_codigo 	CodigoAluno,
+							cc.id 				CodigoComponenteCurricular,
+							coalesce(
+								cc.descricao_sgp, 
+								cc.descricao) 	ComponenteCurricular,
+							coalesce (
+								ccn.nota::varchar, cv2.valor,
+								fn.nota::varchar, cv.valor,
+								sv.valor,
+								''
+							) 					Nota,
+							cca.id 				conselho_classe_aluno_id
+						from 
+							fechamento_turma ft
+						inner join fechamento_turma_disciplina ftd on ftd.fechamento_turma_id = ft.id 
+						inner join fechamento_aluno fa on fa.fechamento_turma_disciplina_id = ftd.id 
+						inner join fechamento_nota fn on fn.fechamento_aluno_id = fa.id 
+						inner join turma t on t.id = ft.turma_id
+						inner join ue on ue.id = t.ue_id 
+						left join periodo_escolar pe on pe.id = ft.periodo_escolar_id 
+						left join componente_curricular cc on cc.id = fn.disciplina_id
+						left join conselho_classe cc2 on cc2.fechamento_turma_id = ft.id 
+						left join conselho_classe_aluno cca on cca.aluno_codigo = fa.aluno_codigo and cca.conselho_classe_id = cc2.id and not cca.excluido 
+						left join conselho_classe_nota ccn on ccn.conselho_classe_aluno_id = cca.id and ccn.componente_curricular_codigo = cc.id and not ccn.excluido 
+						left join conceito_valores cv on cv.id = fn.conceito_id
+						left join conceito_valores cv2 on cv2.id = ccn.conceito_id 
+						left join sintese_valores sv on sv.id = fn.sintese_id 
+						where t.ano_letivo >= 2020
+							and (cca.id is null or (cca.id is not null and ccn.id is not null))
+							and not ftd.excluido 
+							and not fa.excluido 
+							and not fn.excluido 
+						order by 
+							cca.id desc
                         ");
                 conexao.Close();
 
