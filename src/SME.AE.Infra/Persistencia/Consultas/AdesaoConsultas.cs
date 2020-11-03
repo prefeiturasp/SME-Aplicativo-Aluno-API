ca338ffedeca174e0316e17f116a1f16bbaf4825
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-11-03 19:31:32 -0300

    melhoria no filtro para worker de consolidacao

--- src/SME.AE.Infra/Persistencia/Consultas/AdesaoConsultas.cs
+++ src/SME.AE.Infra/Persistencia/Consultas/AdesaoConsultas.cs
@@ -7,24 +7,29 @@
 			concat(dre_codigo,'-',dre_nome) AS NomeCompletoDre,
 			concat(ue_codigo,'-',ue_nome) AS NomeCompletoUe,
 			codigo_turma AS CodigoTurma, 
-			sum(usuarios_cpf_invalidos) AS TotalUsuariosComCpfInvalidos,
-			sum(usuarios_sem_app_instalado) AS TotalUsuariosSemAppInstalado, 
-			sum(usuarios_primeiro_acesso_incompleto) AS TotalUsuariosPrimeiroAcessoIncompleto, 
-			sum(usuarios_validos) AS TotalUsuariosValidos 
+				usuarios_cpf_invalidos AS TotalUsuariosComCpfInvalidos,
+				usuarios_sem_app_instalado AS TotalUsuariosSemAppInstalado, 
+				usuarios_primeiro_acesso_incompleto AS TotalUsuariosPrimeiroAcessoIncompleto, 
+				usuarios_validos AS TotalUsuariosValidos 
 		FROM dashboard_adesao da  
-		WHERE 1=1 ";
+			WHERE 
+				codigo_turma = 0 and
+				dre_codigo = @dre_codigo and
+				ue_codigo  = @ue_codigo
+		";
 
 		internal static string ObterDadosAdesaoSme = @"
-		SELECT 	
-			null AS NomeCompletoDre,
-			null AS NomeCompletoUe,
-			null AS CodigoTurma, 
-			sum(usuarios_cpf_invalidos) AS TotalUsuariosComCpfInvalidos,
-			sum(usuarios_sem_app_instalado) AS TotalUsuariosSemAppInstalado, 
-			sum(usuarios_primeiro_acesso_incompleto) AS TotalUsuariosPrimeiroAcessoIncompleto, 
-			sum(usuarios_validos) AS TotalUsuariosValidos 
-		FROM dashboard_adesao da  
-		WHERE 1=1 ";
+			SELECT 
+				usuarios_cpf_invalidos AS TotalUsuariosComCpfInvalidos,
+				usuarios_sem_app_instalado AS TotalUsuariosSemAppInstalado, 
+				usuarios_primeiro_acesso_incompleto AS TotalUsuariosPrimeiroAcessoIncompleto, 
+				usuarios_validos AS TotalUsuariosValidos 
+			FROM dashboard_adesao da  
+			WHERE 
+				codigo_turma = 0 and
+				dre_codigo = '' and
+				ue_codigo  = ''
+		";
 
 	}
 }
