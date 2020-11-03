8a9d31534bb6cbb17b566f1cc096c3e0cf46ae84
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-11-03 20:50:40 -0300

    Alteracao para merge

--- src/SME.AE.Infra/Persistencia/Consultas/AdesaoConsultas.cs
+++ src/SME.AE.Infra/Persistencia/Consultas/AdesaoConsultas.cs
@@ -31,5 +31,29 @@
 				ue_codigo  = ''
 		";
 
+		internal static string ObterDadosAdesaoAgrupadosPorDreUe = @"
+		SELECT 	
+			concat(dre_codigo,'-',dre_nome) AS NomeCompletoDre,
+			null AS NomeCompletoUe,
+			null AS CodigoTurma, 
+			sum(usuarios_cpf_invalidos) AS TotalUsuariosComCpfInvalidos,
+			sum(usuarios_sem_app_instalado) AS TotalUsuariosSemAppInstalado, 
+			sum(usuarios_primeiro_acesso_incompleto) AS TotalUsuariosPrimeiroAcessoIncompleto, 
+			sum(usuarios_validos) AS TotalUsuariosValidos 
+		FROM dashboard_adesao da  
+		WHERE 1=1 ";
+
+		internal static string ObterDadosAdesaoSintetico = @"
+		SELECT 	
+			null AS NomeCompletoDre,
+			null AS NomeCompletoUe,
+			null AS CodigoTurma, 
+			sum(usuarios_cpf_invalidos) AS TotalUsuariosComCpfInvalidos,
+			sum(usuarios_sem_app_instalado) AS TotalUsuariosSemAppInstalado, 
+			sum(usuarios_primeiro_acesso_incompleto) AS TotalUsuariosPrimeiroAcessoIncompleto, 
+			sum(usuarios_validos) AS TotalUsuariosValidos 
+		FROM dashboard_adesao da  
+		WHERE 1=1 ";
+
 	}
 }
