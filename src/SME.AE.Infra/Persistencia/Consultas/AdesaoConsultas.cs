4c2b9a9041dfcb1dfe470ca54893dad4f7fbf73c
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-11-03 21:22:42 -0300

    Verificado merge com dev.

--- src/SME.AE.Infra/Persistencia/Consultas/AdesaoConsultas.cs
+++ src/SME.AE.Infra/Persistencia/Consultas/AdesaoConsultas.cs
@@ -2,7 +2,7 @@
 {
     public static class AdesaoConsultas
     {
-        internal static string ObterDadosAdesaoAgrupadosPorDreUe = @"
+        internal static string ObterDadosAdesao = @"
 		SELECT 	
 			concat(dre_codigo,'-',dre_nome) AS NomeCompletoDre,
 			concat(ue_codigo,'-',ue_nome) AS NomeCompletoUe,
@@ -18,8 +18,9 @@
 				ue_codigo  = @ue_codigo
 		";
 
-		internal static string ObterDadosAdesaoSme = @"
+		internal static string ObterDadosAdesaoPorDre = @"
 			SELECT 
+				concat(dre_codigo,'-',dre_nome) AS NomeCompletoDre,
 				usuarios_cpf_invalidos AS TotalUsuariosComCpfInvalidos,
 				usuarios_sem_app_instalado AS TotalUsuariosSemAppInstalado, 
 				usuarios_primeiro_acesso_incompleto AS TotalUsuariosPrimeiroAcessoIncompleto, 
@@ -27,33 +28,8 @@
 			FROM dashboard_adesao da  
 			WHERE 
 				codigo_turma = 0 and
-				dre_codigo = '' and
-				ue_codigo  = ''
+				ue_codigo  = '' and
+				dre_codigo <> '' 
 		";
-
-		internal static string ObterDadosAdesaoAgrupadosPorDreUe = @"
-		SELECT 	
-			concat(dre_codigo,'-',dre_nome) AS NomeCompletoDre,
-			null AS NomeCompletoUe,
-			null AS CodigoTurma, 
-			sum(usuarios_cpf_invalidos) AS TotalUsuariosComCpfInvalidos,
-			sum(usuarios_sem_app_instalado) AS TotalUsuariosSemAppInstalado, 
-			sum(usuarios_primeiro_acesso_incompleto) AS TotalUsuariosPrimeiroAcessoIncompleto, 
-			sum(usuarios_validos) AS TotalUsuariosValidos 
-		FROM dashboard_adesao da  
-		WHERE 1=1 ";
-
-		internal static string ObterDadosAdesaoSintetico = @"
-		SELECT 	
-			null AS NomeCompletoDre,
-			null AS NomeCompletoUe,
-			null AS CodigoTurma, 
-			sum(usuarios_cpf_invalidos) AS TotalUsuariosComCpfInvalidos,
-			sum(usuarios_sem_app_instalado) AS TotalUsuariosSemAppInstalado, 
-			sum(usuarios_primeiro_acesso_incompleto) AS TotalUsuariosPrimeiroAcessoIncompleto, 
-			sum(usuarios_validos) AS TotalUsuariosValidos 
-		FROM dashboard_adesao da  
-		WHERE 1=1 ";
-
 	}
 }
