e8e12394c33e9b95784116a77281fb3b1b635f93
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-11-08 02:26:33 -0300

    Inclusao dos dias de ausencia dos alunos

--- src/SME.AE.Infra/Persistencia/Repositorios/FrequenciaAlunoSgpRepositorio.cs
+++ src/SME.AE.Infra/Persistencia/Repositorios/FrequenciaAlunoSgpRepositorio.cs
@@ -18,36 +18,100 @@ namespace SME.AE.Infra.Persistencia.Repositorios
 
         public async Task<IEnumerable<FrequenciaAlunoSgpDto>> ObterFrequenciaAlunoSgp(int desdeAnoLetivo)
         {
+            const string sqlSelect = @"
+				select
+					t.ano_letivo AnoLetivo,
+					ue.ue_id CodigoUe,
+					ue.nome NomeUe,
+					t.turma_id CodigoTurma,
+					t.nome NomeTurma,
+					fa.codigo_aluno CodigoAluno,
+					fa.bimestre Bimestre,
+					coalesce (cc.descricao_sgp, cc.descricao) ComponenteCurricular,
+					cc.id  CodigoComponenteCurricular,
+					total_aulas QuantidadeAulas,
+					total_ausencias QuantidadeAusencias,
+					total_compensacoes QuantidadeCompensacoes,
+        				(
+        					select array_to_string(array(select distinct to_char(a.data_aula, 'YYYY-MM-DD') 
+							from registro_frequencia rf
+							inner join aula a on a.id = rf.aula_id
+							inner join periodo_escolar pe on a.data_aula between pe.periodo_inicio and pe.periodo_fim 
+							inner join registro_ausencia_aluno raa on raa.registro_frequencia_id = rf.id 
+							where 
+								fa.codigo_aluno = raa.codigo_aluno 
+							and a.turma_id = t.turma_id 
+							and fa.periodo_escolar_id = pe.id 
+							and a.disciplina_id = fa.disciplina_id 
+							),',')
+						) DiasAusencias
+				from frequencia_aluno fa 
+				inner join turma t on t.turma_id = fa.turma_id 
+				inner join ue on ue.id  = t.ue_id 
+				left join componente_curricular cc on cc.id::varchar = fa.disciplina_id
+				where
+					cc.permite_registro_frequencia
+				and	fa.tipo = 1
+				and (t.ano_letivo >= 2020 and t.ano_letivo >= @desdeAnoLetivo)
+				union 
+				select 
+					AnoLetivo,
+					CodigoUe,
+					NomeUe,
+					CodigoTurma,
+					NomeTurma,
+					'' CodigoAluno,
+					Bimestre,
+					ComponenteCurricular,
+					CodigoComponenteCurricular,
+					max(QuantidadeAulas) QuantidadeAulas,
+					0 QuantidadeAusencias,
+					0 QuantidadeCompensacoes,
+					'' DiasAusencias
+				from (
+				select 
+					t.ano_letivo AnoLetivo,
+					ue.ue_id CodigoUe,
+					ue.nome NomeUe,
+					t.turma_id CodigoTurma,
+					t.nome NomeTurma,
+					'' CodigoAluno,
+					pe.bimestre Bimestre,
+					coalesce (cc.descricao_sgp, cc.descricao) ComponenteCurricular,
+					cc.id  CodigoComponenteCurricular,
+					a.quantidade QuantidadeAulas,
+					0 QuantidadeAusencias,
+					0 QuantidadeCompensacoes,
+					'' DiasAusencias
+				from registro_frequencia rf
+				inner join aula a on a.id = rf.aula_id
+				inner join periodo_escolar pe on a.data_aula between pe.periodo_inicio and pe.periodo_fim 
+				inner join turma t on t.turma_id = a.turma_id 
+				inner join ue on ue.id  = t.ue_id 
+				left join componente_curricular cc on cc.id::varchar = a.disciplina_id
+				where
+					cc.permite_registro_frequencia
+				and (t.ano_letivo >= 2020 and t.ano_letivo >= @desdeAnoLetivo)
+				) aaa 
+				group by 
+					AnoLetivo,
+					CodigoUe,
+					NomeUe,
+					CodigoTurma,
+					NomeTurma,
+					Bimestre,
+					ComponenteCurricular,
+					CodigoComponenteCurricular
+            ";
             try
             {
                 using var conexao = CriaConexao();
                 conexao.Open();
 
-                var frequenciaAlunosSgp = await conexao
-                    .QueryAsync<FrequenciaAlunoSgpDto>(
-                        @"
-                        select 
-	                        t.ano_letivo AnoLetivo,
-	                        ue.ue_id CodigoUe,
-	                        ue.nome NomeUe,
-	                        t.turma_id::int8 CodigoTurma,
-	                        t.nome NomeTurma,
-	                        fa.codigo_aluno CodigoAluno,
-	                        fa.bimestre Bimestre,
-	                        coalesce (cc.descricao_sgp, cc.descricao) ComponenteCurricular,
-	                        total_aulas QuantidadeAulas,
-	                        total_ausencias QuantidadeAusencias,
-	                        total_compensacoes QuantidadeCompensacoes
-                        from frequencia_aluno fa 
-                        inner join turma t on t.turma_id = fa.turma_id 
-                        inner join ue on ue.id  = t.ue_id 
-                        left join componente_curricular cc on cc.id::varchar = fa.disciplina_id 
-                        where 
-	                        cc.permite_registro_frequencia
-	                        and (t.ano_letivo >= 2020 and t.ano_letivo >= @desdeAnoLetivo)
-                        order by 
-	                        ue.ue_id, t.turma_id, t.ano_letivo, fa.bimestre, fa.codigo_aluno 	
-                        ", new { desdeAnoLetivo });
+                var frequenciaAlunosSgp = 
+					await conexao
+                    .QueryAsync<FrequenciaAlunoSgpDto>(sqlSelect, new { desdeAnoLetivo });
+
                 conexao.Close();
 
                 return frequenciaAlunosSgp;
