9533a40aec06bc4a3a4d6846b5631e551c472997
Carlos Dias <carlos.dias@amcom.com.br>
2020-11-11 11:27:16 -0300

    Ajuste do script para criação de coluna se não existir e limpeza da tabela nota_aluno_cor antes da inserção. AB#24610

--- scripts/V32__Inclusao_Tabela_Nota_Aluno_Cor.sql
+++ scripts/V32__Inclusao_Tabela_Nota_Aluno_Cor.sql
@@ -5,11 +5,11 @@ cor VARCHAR(7) not null);
 CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS nota_aluno_cor_nota_idx 
 ON public.nota_aluno_cor (nota);
 
-    
+delete from nota_aluno_cor;
 insert into nota_aluno_cor (nota, cor) values ('NS', '#F6461F');
 insert into nota_aluno_cor (nota, cor) values ('PS', '#74C908');
 insert into nota_aluno_cor (nota, cor) values ('NotaAbaixo5', '#F6461F');
 insert into nota_aluno_cor (nota, cor) values ('NotaEntre7e5', '#F5D00A');
 insert into nota_aluno_cor (nota, cor) values ('NotaAcimaDe75', '#74C908');
 
-alter table nota_aluno add nota_descricao varchar(50) null;
+alter table nota_aluno add if not exists nota_descricao varchar(50) null;
