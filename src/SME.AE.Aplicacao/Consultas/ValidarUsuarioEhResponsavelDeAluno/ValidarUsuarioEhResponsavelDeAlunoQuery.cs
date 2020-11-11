1a10534b78639299d7d909e65effbe050bd7c085
Rodrigo Galante <rodrigo.galante@globo.com>
2020-11-11 09:28:03 -0300

    Implementação de serviço que verifica se o cpf do usuário tem aluno no qual é responsável

---
+++ src/SME.AE.Aplicacao/Consultas/ValidarUsuarioEhResponsavelDeAluno/ValidarUsuarioEhResponsavelDeAlunoQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace SME.AE.Aplicacao.Consultas
+{
+    public class ValidarUsuarioEhResponsavelDeAlunoQuery : IRequest<bool>
+    {
+        public string Cpf { get; set; }
+
+        public ValidarUsuarioEhResponsavelDeAlunoQuery(string cpf)
+        {
+            Cpf = cpf;
+        }
+    }
+}
