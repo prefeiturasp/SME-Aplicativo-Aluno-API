82da47da9710785e21fb2271d8d0adbe4b5538e8
Carlos Dias <carlos.dias@amcom.com.br>
2020-11-13 16:05:39 -0300

    Adição da relação de dias de ausência do aluno no bimestre AB#15860

---
+++ src/SME.AE.Aplicacao/Comum/Modelos/Resposta/FrequenciasDoAluno/PorComponenteCurricular/FrequenciaAlunoPorBimestreAusencia.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SME.AE.Aplicacao.Comum.Modelos.Resposta.FrequenciasDoAluno.PorComponenteCurricular
+{
+    public class FrequenciaAlunoPorBimestreAusencia
+    {
+        public DateTime Data { get; set; }
+        public int QuantidadeDeFaltas { get; set; }
+    }
+}
