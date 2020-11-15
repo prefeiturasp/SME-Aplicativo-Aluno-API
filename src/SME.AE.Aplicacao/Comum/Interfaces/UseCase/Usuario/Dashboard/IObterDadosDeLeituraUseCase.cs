d31d30b4ec894b6c2984f6c39d1bf99144a98d36
Rodrigo Galante <rodrigo.galante@globo.com>
2020-11-15 00:55:55 -0300

    Implementação parcial

---
+++ src/SME.AE.Aplicacao/Comum/Interfaces/UseCase/Usuario/Dashboard/IObterDadosDeLeituraUseCase.cs
@@ -0,0 +1,13 @@
+﻿using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Comum.Interfaces.UseCase.Usuario.Dashboard
+{
+    public interface IObterDadosDeLeituraComunicadosUseCase
+    {
+        Task<IEnumerable<DadosLeituraResultado>> Executar(string codigoDre, string codigoUe);
+    }
+
+    
+}
---
+++ src/SME.AE.Aplicacao/Consultas/ObterDadosLeituraComunicados/ObterDadosLeituraComunicadosQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using System.Collections.Generic;
+
+namespace SME.AE.Aplicacao.Consultas.ObterDadosLeituraComunicados
+{
+    public class ObterDadosLeituraComunicadosQuery : IRequest<IEnumerable<DadosLeituraResultado>>
+    {
+        public string CodigoDre { get; set; }
+
+        public string CodigoUe { get; set; }
+
+        public ObterDadosLeituraComunicadosQuery(string codigoDre, string codigoUe)
+        {
+            CodigoDre = codigoDre;
+            CodigoUe = codigoUe;
+        }
+    }
+}
