40e976a47e9b9b7ff87fc144f4c2e188525d034e
Carlos Dias <carlos.dias@amcom.com.br>
2020-11-17 12:47:03 -0300

    Alteração da identificação da nota na tabela AB#24610

--- scripts/V33__Inclusao_Index_E_Registros_Tabela_Nota_Aluno_Cor.sql
+++ scripts/V33__Inclusao_Index_E_Registros_Tabela_Nota_Aluno_Cor.sql
@@ -3,4 +3,4 @@ insert into nota_aluno_cor (nota, cor) values ('NS', '#F6461F');
 insert into nota_aluno_cor (nota, cor) values ('PS', '#74C908');
 insert into nota_aluno_cor (nota, cor) values ('NotaAbaixo5', '#F6461F');
 insert into nota_aluno_cor (nota, cor) values ('NotaEntre7e5', '#F5D00A');
-insert into nota_aluno_cor (nota, cor) values ('NotaAcimaDe75', '#74C908');
+insert into nota_aluno_cor (nota, cor) values ('NotaAcimaDe7', '#74C908');
