83ef93f4321d9ead32818bd40f546b12199bdfb0
Carlos Dias <carlos.dias@amcom.com.br>
2020-11-17 12:45:28 -0300

    Remoção do cache na cosulta das cores para nota do aluno AB#24610

--- src/SME.AE.Infra/Persistencia/Repositorios/NotaAlunoCorRepositorio.cs
+++ src/SME.AE.Infra/Persistencia/Repositorios/NotaAlunoCorRepositorio.cs
@@ -13,24 +13,12 @@ namespace SME.AE.Infra.Persistencia.Repositorios
 {
     public class NotaAlunoCorRepositorio : INotaAlunoCorRepositorio
     {
-        private readonly ICacheRepositorio _cacheRepositorio;
-        private const string ChaveCache = "notaAlunoCores";
-
         private NpgsqlConnection CriaConexao() => new NpgsqlConnection(ConnectionStrings.Conexao);
-
-        public NotaAlunoCorRepositorio(ICacheRepositorio cacheRepositorio)
-        {
-            _cacheRepositorio = cacheRepositorio;
-        }
 
         public async Task<IEnumerable<NotaAlunoCor>> ObterAsync()
         {
             try
             {
-                var notasAluno = await _cacheRepositorio.ObterAsync(ChaveCache);
-                if (!string.IsNullOrWhiteSpace(notasAluno))
-                    return JsonConvert.DeserializeObject<IEnumerable<NotaAlunoCor>>(notasAluno);
-
                 using var conexao = CriaConexao();
                 conexao.Open();
                 var dadosNotasAluno = await conexao.QueryAsync<NotaAlunoCor>(@"SELECT
@@ -38,8 +26,6 @@ namespace SME.AE.Infra.Persistencia.Repositorios
                                                                                 cor AS Cor
                                                                              FROM public.nota_aluno_cor");
                 conexao.Close();
-
-                await _cacheRepositorio.SalvarAsync(ChaveCache, dadosNotasAluno, 720, false);
                 return dadosNotasAluno;
             }
             catch (Exception ex)
