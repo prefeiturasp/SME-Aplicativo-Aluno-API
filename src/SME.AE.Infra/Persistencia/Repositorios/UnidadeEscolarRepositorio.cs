563e41234ccbb3388fe25c0b651a048d9279cb64
Rodrigo Galante <rodrigo.galante@globo.com>
2020-11-25 00:51:54 -0300

    inclusao de email e telefone da UE

--- src/SME.AE.Infra/Persistencia/Repositorios/UnidadeEscolarRepositorio.cs
+++ src/SME.AE.Infra/Persistencia/Repositorios/UnidadeEscolarRepositorio.cs
@@ -35,22 +35,57 @@ namespace SME.AE.Infra.Persistencia.Repositorios
                 using var conexao = CriaConexao();
                 conexao.Open();
 
-                var query = @"select 
-	                            distinct
-	                            concat(ue.cd_unidade_educacao,'-', ue.nm_exibicao_unidade) nomeCompletoUe, 
-	                            tp.dc_tp_logradouro tipoLogradouro,
-	                            ue.nm_logradouro logradouro, 
-	                            ue.cd_nr_endereco numero,
-	                            ue.nm_bairro bairro, 
-	                            ue.cd_cep cep, 
-	                            mun.nm_municipio municipio,
-	                            mun.sg_uf uf,
-	                            '' email,
-	                            '' telefone
-                            from v_cadastro_unidade_educacao  ue
-                            inner join municipio mun on mun.cd_municipio = ue.cd_municipio 
-                            inner join tipo_logradouro tp on tp.tp_logradouro = ue.tp_logradouro 
-                            where cd_unidade_educacao = @codigoUe ";
+                var query = @"SELECT distinct
+                               concat(ue.cd_unidade_educacao, '-', ue.nm_exibicao_unidade) nomeCompletoUe,
+                               tp.dc_tp_logradouro tipoLogradouro,
+                               RTRIM(LTRIM(ue.nm_logradouro)) logradouro,
+                               RTRIM(LTRIM(ue.cd_nr_endereco)) numero,
+                               ue.nm_bairro bairro,
+                               ue.cd_cep cep,
+                               mun.nm_municipio municipio,
+                               mun.sg_uf uf,
+                               (
+                                  select
+                                     dcu.dc_dispositivo as email 
+                                  from
+                                     v_cadastro_unidade_educacao cue 
+                                     inner join
+                                        dispositivo_comunicacao_unidade dcu 
+                                        on cue.cd_unidade_educacao = dcu.cd_unidade_educacao 
+                                     inner join
+                                        tipo_dispositivo_comunicacao tdc 
+                                        on dcu.tp_dispositivo_comunicacao = tdc.tp_dispositivo_comunicacao 
+                                  where
+                                     cue.cd_unidade_educacao = @codigoUe 
+                                     and tdc.tp_dispositivo_comunicacao = 9 
+                               )
+                               email,
+                               (
+                                  select
+                                     concat(REPLACE(dcu.cd_ddd, '00', '0'), dcu.dc_dispositivo) as telefone 
+                                  from
+                                     v_cadastro_unidade_educacao cue 
+                                     inner join
+                                        dispositivo_comunicacao_unidade dcu 
+                                        on cue.cd_unidade_educacao = dcu.cd_unidade_educacao 
+                                     inner join
+                                        tipo_dispositivo_comunicacao tdc 
+                                        on dcu.tp_dispositivo_comunicacao = tdc.tp_dispositivo_comunicacao 
+                                  where
+                                     cue.cd_unidade_educacao = @codigoUe 
+                                     and tdc.tp_dispositivo_comunicacao = 1 
+                               )
+                               telefone 
+                            from
+                               v_cadastro_unidade_educacao ue 
+                               inner join
+                                  municipio mun 
+                                  on mun.cd_municipio = ue.cd_municipio 
+                               inner join
+                                  tipo_logradouro tp 
+                                  on tp.tp_logradouro = ue.tp_logradouro 
+                            where
+                               cd_unidade_educacao = @codigoUe ";
 
                 var parametros = new { codigoUe };
                 var dadosUnidadeEscolar = await conexao.QuerySingleAsync<UnidadeEscolarResposta>(query, parametros);
