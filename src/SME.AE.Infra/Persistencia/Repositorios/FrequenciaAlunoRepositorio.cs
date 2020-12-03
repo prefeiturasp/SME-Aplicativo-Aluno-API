b60ff45169fb8eb6e2b1601eb8d944d16a0e6b3b
Rodrigo Galante <rodrigo.galante@globo.com>
2020-12-03 00:53:37 -0300

    Ajuste na exibição das cores de frequência

--- src/SME.AE.Infra/Persistencia/Repositorios/FrequenciaAlunoRepositorio.cs
+++ src/SME.AE.Infra/Persistencia/Repositorios/FrequenciaAlunoRepositorio.cs
@@ -36,11 +36,11 @@ namespace SME.AE.Infra.Persistencia.Repositorios
         {
             try
             {
-                //var chaveCache = chaveCacheAnoUeTurmaAlunoComponenteCurricular(anoLetivo, codigoUe, codigoTurma, codigoAluno, codigoComponenteCurricular);
+                var chaveCache = chaveCacheAnoUeTurmaAlunoComponenteCurricular(anoLetivo, codigoUe, codigoTurma, codigoAluno, codigoComponenteCurricular);
 
-                //var frequenciaAluno = await cacheRepositorio.ObterAsync(chaveCache);
-                //if (!string.IsNullOrWhiteSpace(frequenciaAluno))
-                //    return JsonConvert.DeserializeObject<FrequenciaAlunoPorComponenteCurricularResposta>(frequenciaAluno);
+                var frequenciaAluno = await cacheRepositorio.ObterAsync(chaveCache);
+                if (!string.IsNullOrWhiteSpace(frequenciaAluno))
+                    return JsonConvert.DeserializeObject<FrequenciaAlunoPorComponenteCurricularResposta>(frequenciaAluno);
 
                 using var conexao = CriaConexao();
                 conexao.Open();
@@ -103,7 +103,7 @@ namespace SME.AE.Infra.Persistencia.Repositorios
                     query, x => x.CodigoComponenteCurricular, x => x.FrequenciasPorBimestre, parametros, splitOn: "splitOn");
                 conexao.Close();
 
-                //await cacheRepositorio.SalvarAsync(chaveCache, dadosFrequenciaAlunos, 720, false);
+                await cacheRepositorio.SalvarAsync(chaveCache, dadosFrequenciaAlunos, 720, false);
                 return dadosFrequenciaAlunos;
             }
             catch (Exception ex)
@@ -128,7 +128,17 @@ namespace SME.AE.Infra.Persistencia.Repositorios
 
                 var query = @"DROP TABLE IF EXISTS tmp_frequencia_aluno_global;
 
-                            SELECT
+                            select 
+                                AnoLetivo,
+                                CodigoUe,
+                                CodigoTurma,
+                                AlunoCodigo,
+                                SUM(QuantidadeAulas) as QuantidadeAulas,
+                                SUM(QuantidadeFaltas) as QuantidadeFaltas,
+                                SUM(QuantidadeCompensacoes) as QuantidadeCompensacoes
+                                INTO temp tmp_frequencia_aluno_global
+                            from 
+                            (SELECT
                                 ano_letivo as AnoLetivo,
                                 ue_codigo as CodigoUe,
                                 turma_codigo as CodigoTurma,
@@ -136,18 +146,47 @@ namespace SME.AE.Infra.Persistencia.Repositorios
                                 SUM(quantidade_aulas) as QuantidadeAulas,
                                 SUM(quantidade_faltas) as QuantidadeFaltas,
                                 SUM(quantidade_compensacoes) as QuantidadeCompensacoes
-                            INTO temp tmp_frequencia_aluno_global
                             FROM public.frequencia_aluno
                             WHERE
-	                            ano_letivo = @anoLetivo
+                                ano_letivo = @anoLetivo
                                 AND ue_codigo = @codigoUe
                                 AND turma_codigo = @codigoTurma
-                                AND aluno_codigo = @codigoAluno
+   	                            AND aluno_codigo = @codigoAluno
                             GROUP BY
                                 ano_letivo,
                                 ue_codigo,
                                 turma_codigo,
-                                aluno_codigo;
+                                aluno_codigo
+                             union
+                            SELECT
+                                ano_letivo as AnoLetivo,
+                                ue_codigo as CodigoUe,
+                                turma_codigo as CodigoTurma,
+                                @codigoAluno as AlunoCodigo,
+                                SUM(quantidade_aulas) as QuantidadeAulas,
+                                SUM(quantidade_faltas) as QuantidadeFaltas,
+                                SUM(quantidade_compensacoes) as QuantidadeCompensacoes
+                            FROM public.frequencia_aluno a
+                            WHERE
+                                ano_letivo = @anoLetivo
+                                AND ue_codigo = @codigoUe
+                                AND turma_codigo = @codigoTurma
+   	                            AND aluno_codigo = ''
+	                            and not exists (select 1 FROM public.frequencia_aluno b WHERE a.ano_letivo = b.ano_letivo
+    				                            AND a.ue_codigo = b.ue_codigo 
+    				                            AND a.turma_codigo = b.turma_codigo
+    				                            and a.bimestre = b.bimestre 
+   					                            AND aluno_codigo = @codigoAluno)   	   	
+                            GROUP BY
+                                ano_letivo,
+                                ue_codigo,
+                                turma_codigo,
+                                aluno_codigo) aaa
+                            GROUP BY
+                                AnoLetivo,
+                                CodigoUe,
+                                CodigoTurma,
+                                AlunoCodigo;
 
                             DROP TABLE IF EXISTS tmp_frequencia_aluno_componentes_curriculares;
                             SELECT
