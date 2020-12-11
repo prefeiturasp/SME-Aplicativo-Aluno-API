0832db1b5cbec84ee370a67b9216d5eb713688b5
Rodrigo Galante <rodrigo.galante@globo.com>
2020-12-11 17:11:07 -0300

    refactory para retornar dados da modalidade, filtrando por mais de uma modalidade e turma específica

--- src/SME.AE.Aplicacao/Consultas/ObterDadosLeituraModalidade/ObterDadosLeituraModalidadeHandler.cs
+++ src/SME.AE.Aplicacao/Consultas/ObterDadosLeituraModalidade/ObterDadosLeituraModalidadeHandler.cs
@@ -21,10 +21,43 @@ namespace SME.AE.Aplicacao.Consultas.ObterDadosLeituraComunicados
 
         public async Task<IEnumerable<DadosLeituraComunicadosPorModalidadeTurmaResultado>> Handle(ObterDadosLeituraModalidadeQuery request, CancellationToken cancellationToken)
         {
-            var dadosLeituraComunicados =  await dadosLeituraRepositorio
+            var dadosLeituraComunicados = await dadosLeituraRepositorio
                     .ObterDadosLeituraModalidade(request.CodigoDre, request.CodigoUe, request.NotificaoId, request.ModoVisualizacao == ModoVisualizacao.Responsavel);
+            
+            return ObterNomeModalidade(dadosLeituraComunicados);
+        }
 
-            return dadosLeituraComunicados;
+        private static List<DadosLeituraComunicadosPorModalidadeTurmaResultado> ObterNomeModalidade(IEnumerable<DadosLeituraComunicadosPorModalidadeTurmaResultado> dadosLeituraComunicados)
+        {
+            var resultado = new List<DadosLeituraComunicadosPorModalidadeTurmaResultado>();
+            foreach (var item in dadosLeituraComunicados)
+            {
+                item.Modalidade = ObterModalidade(item.ModalidadeCodigo);
+                resultado.Add(item);
+            }
+            return resultado;
+        }
+
+        private static string ObterModalidade(string modalidade)
+        {
+            switch (modalidade)
+            {
+                case "1":
+                    modalidade = ModalidadeDeEnsino.Infantil.ToString();
+                    break;
+                case "5":
+                    modalidade = ModalidadeDeEnsino.Fundamental.ToString();
+                    break;
+                case "6":
+                    modalidade = ModalidadeDeEnsino.Medio.ToString();
+                    break;
+                case "3":
+                    modalidade = ModalidadeDeEnsino.EJA.ToString();
+                    break;
+                default:
+                    break;
+            }
+            return modalidade;
         }
 
     }
