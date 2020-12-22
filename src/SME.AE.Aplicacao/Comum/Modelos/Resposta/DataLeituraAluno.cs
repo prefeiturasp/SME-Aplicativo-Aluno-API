e1b8e8559cf835654f1019d0d93770b7a1c56405
Ivano Mucci <ivano.mucci@amcom.com.br>
2020-12-22 18:48:32 -0300

    criacao do endpoint de leitura por alunos

---
+++ src/SME.AE.Aplicacao/Comum/Modelos/Resposta/DataLeituraAluno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.AE.Aplicacao.Comum.Modelos.Resposta
+{
+    public class DataLeituraAluno
+    {
+        public long CodigoAluno { get; set; }
+        public DateTime? DataLeitura { get; set; }
+    }
+}
