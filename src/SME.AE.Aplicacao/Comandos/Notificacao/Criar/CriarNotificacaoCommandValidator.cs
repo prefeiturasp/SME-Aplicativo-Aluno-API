1f56cd66d415d8ab46a62dde34a214285049f27d
Rodrigo Galante <rodrigo.galante@globo.com>
2021-02-01 14:54:06 -0300

    Correção feita na master para tratar número máximo de caracteres

---
+++ src/SME.AE.Aplicacao/Comandos/Notificacao/Criar/CriarNotificacaoCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace SME.AE.Aplicacao.Comandos.Notificacao.Criar
+{
+    public class CriarNotificacaoCommandValidator : AbstractValidator<CriarNotificacaoCommand>
+    {
+        public CriarNotificacaoCommandValidator()
+        {
+            RuleFor(x => x.Notificacao.Id).NotEmpty().WithMessage("O ID da notificação é obrigatório");
+            RuleFor(x => x.Notificacao.DataEnvio).NotEmpty().WithMessage("A Data de envio da notificação é obrigatória");
+            RuleFor(x => x.Notificacao.DataExpiracao).NotEmpty().WithMessage("A Data de expiração da notificação é obrigatória");
+            RuleFor(x => x.Notificacao.Titulo).NotEmpty().WithMessage("O Título da notificação é obrigatório");
+            RuleFor(x => x.Notificacao.Mensagem).NotEmpty().WithMessage("A Mensagem da notificação é obrigatória");
+            RuleFor(x => x.Notificacao.Mensagem).Length(1, 1024).WithMessage("A Mensagem da notificação deve conter no máximo 1024 caracteres");
+        }
+    }
+}
