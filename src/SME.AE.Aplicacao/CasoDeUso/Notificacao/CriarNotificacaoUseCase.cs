f007732733a16d116016cea0d22d98a93069abaa
Rodrigo Galante <rodrigo.galante@globo.com>
2021-02-01 14:54:06 -0300

    Correção feita na master para tratar número máximo de caracteres

--- src/SME.AE.Aplicacao/CasoDeUso/Notificacao/CriarNotificacaoUseCase.cs
+++ src/SME.AE.Aplicacao/CasoDeUso/Notificacao/CriarNotificacaoUseCase.cs
@@ -66,7 +66,7 @@ namespace SME.AE.Aplicacao
             return new Dictionary<String, String>
             {
                 ["Titulo"] = notificacao.Titulo,
-                ["Mensagem"] = notificacao.Mensagem,
+                ["Mensagem"] = notificacao.Mensagem.Length > 1024 ? notificacao.Mensagem.Substring(0, 1024) : notificacao.Mensagem,
                 ["categoriaNotificacao"] = notificacao.CategoriaNotificacao,
                 ["Id"] = notificacao.Id.ToString(),
                 ["CriadoEm"] = notificacao.CriadoEm.ToString("yyyy-MM-dd HH:mm:ss.ffffff"),
