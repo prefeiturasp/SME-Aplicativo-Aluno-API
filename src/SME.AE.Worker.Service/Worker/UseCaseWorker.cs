53ac8f669b0e61ce3e275b3d2fbf4c1d75c65ad9
Rodrigo Galante <rodrigo.galante@globo.com>
2021-02-05 10:46:35 -0300

    inclusão de logs em processos do worker e retirada de usings sem uso

--- src/SME.AE.Worker.Service/Worker/UseCaseWorker.cs
+++ src/SME.AE.Worker.Service/Worker/UseCaseWorker.cs
@@ -59,7 +59,6 @@ namespace SME.AE.Worker.Service
 
         private async Task ExecutaCasoDeUso()
         {
-            //SentrySdk.CaptureException(new Exception($"Executando caso de uso {typeof(T).Name} => {DateTime.Now}"));
             SentrySdk.CaptureMessage($"Executando caso de uso {typeof(T).Name} => {DateTime.Now}");
             logger?.LogInformation($"Executando caso de uso {typeof(T).Name} => {DateTime.Now}");
 
@@ -115,8 +114,8 @@ namespace SME.AE.Worker.Service
                     catch (OperationCanceledException) { }
                     catch (Exception ex)
                     {
-                        //SentrySdk.CaptureMessage("*** Worker error: " + ex.Message);
-                        //SentrySdk.CaptureException(ex);
+                        SentrySdk.CaptureMessage("*** Worker error: " + ex.Message);
+                        SentrySdk.CaptureException(ex);
                         logger?.LogError(ex, "*** Worker error:");
                         throw ex;
                     }
