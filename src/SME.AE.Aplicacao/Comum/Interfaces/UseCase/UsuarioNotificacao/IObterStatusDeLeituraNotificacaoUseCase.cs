411e384805bae24661bf90898de84322f6d62d16
Rodrigo Galante <rodrigo.galante@globo.com>
2021-03-08 12:08:07 -0300

    Implementação de serviço de status de leitura de notificações por aluno

---
+++ src/SME.AE.Aplicacao/Comum/Interfaces/UseCase/UsuarioNotificacao/IObterStatusDeLeituraNotificacaoUseCase.cs
@@ -0,0 +1,11 @@
+﻿using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Comum.Interfaces.UseCase
+{
+    public interface IObterStatusDeLeituraNotificacaoUseCase
+    {
+        Task<IEnumerable<StatusNotificacaoUsuario>> Executar(List<long> notificacaoId, long codigoAluno);
+    }
+}
