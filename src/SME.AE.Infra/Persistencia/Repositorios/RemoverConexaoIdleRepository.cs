e5b1a2111c706efaad61ad192c89e01ad460faf4
Robson <robson.silva@amcom.com.br>
2021-03-23 10:47:38 -0300

    Bug: 34876 Implementação da funcionalidade para remover conexões em idle.

---
+++ src/SME.AE.Infra/Persistencia/Repositorios/RemoverConexaoIdleRepository.cs
@@ -0,0 +1,13 @@
+﻿using Npgsql;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+
+namespace SME.AE.Infra.Persistencia.Repositorios
+{
+    public class RemoverConexaoIdleRepository : IRemoverConexaoIdleRepository
+    {
+        public void RemoverConexoesIdle()
+        {
+            NpgsqlConnection.ClearAllPools();
+        }
+    }
+}
