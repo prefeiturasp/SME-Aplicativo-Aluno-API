2b9550eece19dba09fea7f76da68e79fc83cd763
Marcos Lobo <marcos.lobo.nit@gmail.com>
2021-06-16 18:51:49 -0300

    Inicio fontes do novo worker Normalização do projeto de aplicação

---
+++ src/SME.AE.COMUM/Fila/ExchangeRabbit.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.AE.Comum.Fila
+{
+    public static class ExchangeRabbit
+    {
+        public static string Ae => "sme.ae.workers";
+        public static string AeDeadLetter => "sme.ae.workers.deadletter";
+    }
+}
---
+++ src/SME.AE.COMUM/Fila/RotasRabbitAe.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.AE.Comum.Fila
+{
+    public static class RotasRabbitAe
+    {
+        public const string RotaAtualizacaoCadastralEol = "ae.usuario.cadastro.eol.atualizar";
+        public const string RotaAtualizacaoCadastralProdam = "ae.usuario.cadastro.prodam.atualizar";
+    }
+}
