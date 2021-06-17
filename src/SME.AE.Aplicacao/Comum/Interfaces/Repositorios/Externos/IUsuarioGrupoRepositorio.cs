dfc34277760556fcdcf0721dbf1230696dc38436
Jeferson Cassiano <jeferson.nicolau92@gmail.com>
2021-06-17 16:05:00 -0300

    Ajuste sql

--- src/SME.AE.Aplicacao/Comum/Interfaces/Repositorios/Externos/IUsuarioGrupoRepositorio.cs
+++ src/SME.AE.Aplicacao/Comum/Interfaces/Repositorios/Externos/IUsuarioGrupoRepositorio.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.Data.SqlClient;
-using SME.AE.Dominio.Entidades.Externas;
+﻿using SME.AE.Dominio.Entidades.Externas;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace SME.AE.Aplicacao.Comum.Interfaces.Repositorios.Externos
--- src/SME.AE.Aplicacao/Comum/Interfaces/Repositorios/Externos/IUsuarioSenhaHistoricoCoreSSORepositorio.cs
+++ src/SME.AE.Aplicacao/Comum/Interfaces/Repositorios/Externos/IUsuarioSenhaHistoricoCoreSSORepositorio.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
-using SME.AE.Dominio.Entidades.Externas;
+﻿using SME.AE.Dominio.Entidades.Externas;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace SME.AE.Aplicacao.Comum.Interfaces.Repositorios.Externos
