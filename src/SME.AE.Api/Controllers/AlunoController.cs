e806ad4f73c5a98d6a2487a35b7f64243dda84ca
Caique Siqueira <latorrecaique@gmail.com>
2021-06-25 13:33:15 -0300

    ajuste turmaCodigo

--- src/SME.AE.Api/Controllers/AlunoController.cs
+++ src/SME.AE.Api/Controllers/AlunoController.cs
@@ -48,10 +48,10 @@ namespace SME.AE.Api.Controllers
             return Ok(await useCase.Executar(new NotaConceitoPorBimestresAlunoTurmaDto(ueId, turmaId, alunoCodigo, bimestres)));
         }
 
-        [HttpGet("aluno/turmas/{codigoTurma}/boletins/liberacoes/bimestres")]
-        public async Task<ObjectResult> ObterBimestresLiberacaoDeBoletimAlunoTurma(string codigoTurma, [FromServices] IObterBimestresLiberacaoBoletimAlunoUseCase useCase)
+        [HttpGet("aluno/turmas/{turmaCodigo}/boletins/liberacoes/bimestres")]
+        public async Task<ObjectResult> ObterBimestresLiberacaoDeBoletimAlunoTurma(string turmaCodigo, [FromServices] IObterBimestresLiberacaoBoletimAlunoUseCase useCase)
         {
-            return Ok(await useCase.Executar(codigoTurma));
+            return Ok(await useCase.Executar(turmaCodigo));
         }
     }
 }
