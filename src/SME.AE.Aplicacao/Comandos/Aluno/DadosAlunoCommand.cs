627da1c72ea6dd0c1fcbdb6e2f027dd38ead6ca2
Marcos Lobo <marcos.lobo.nit@gmail.com>
2021-06-28 14:49:28 -0300

    Inicio da modificação do envio e obtenção dos topics

--- src/SME.AE.Aplicacao/Comandos/Aluno/DadosAlunoCommand.cs
+++ src/SME.AE.Aplicacao/Comandos/Aluno/DadosAlunoCommand.cs
@@ -27,60 +27,68 @@ namespace SME.AE.Aplicacao.Comandos.Aluno
 
         public class DadosAlunoComandoHandler : IRequestHandler<DadosAlunoCommand, RespostaApi>
         {
-            private readonly IAlunoRepositorio _repository;
-            private readonly IGrupoComunicadoRepository _repositorioGrupoComunicado;
+            private readonly IAlunoRepositorio alunoRepositorio;
+            private readonly IMediator mediator;
 
-            public DadosAlunoComandoHandler(IAlunoRepositorio repository, IGrupoComunicadoRepository repositorioGrupoComunicado)
+            public DadosAlunoComandoHandler(IAlunoRepositorio alunoRepositorio, IMediator mediator)
             {
-                _repository = repository;
-                _repositorioGrupoComunicado = repositorioGrupoComunicado;
+                this.alunoRepositorio = alunoRepositorio ?? throw new ArgumentNullException(nameof(alunoRepositorio));
+                this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
             }
-            public async Task<RespostaApi> Handle
-             (DadosAlunoCommand request, CancellationToken cancellationToken)
+            public async Task<RespostaApi> Handle(DadosAlunoCommand request, CancellationToken cancellationToken)
             {
-                var grupos = await _repositorioGrupoComunicado.ObterTodos();
-                var resultado = await _repository.ObterDadosAlunos(request.Cpf);
+                //var grupos = await _repositorioGrupoComunicado.ObterTodos();
+                var dadosDosAlunos = await alunoRepositorio.ObterDadosAlunos(request.Cpf);
 
-                if (resultado == null || !resultado.Any())
+                if (dadosDosAlunos == null || !dadosDosAlunos.Any())
                     throw new NegocioException("Este CPF não está relacionado como responsável de um aluno ativo na rede municipal.");
 
-                resultado.ForEach(x => { var g = SelecionarGrupos(x.CodigoTipoEscola, x.CodigoEtapaEnsino, x.CodigoCicloEnsino, grupos); x.Grupo = g.gupo; x.CodigoGrupo = g.codigo; });
+                var turmasCodigo = dadosDosAlunos.Select(a => a.CodigoTurma).Distinct();
 
-                var tipoEscola =
-                    resultado
-                    .GroupBy(g => new { g.Grupo, g.CodigoGrupo })
-                    .Select(s => new ListaEscola
-                    {
-                        Grupo = s.Key.Grupo,
-                        CodigoGrupo = s.Key.CodigoGrupo,
-                        Alunos = resultado
-                                .Where(w => w.CodigoGrupo == s.Key.CodigoGrupo)
-                                .Select(a => new Dominio.Entidades.Aluno
-                                {
-                                    CodigoEol = a.CodigoEol,
-                                    Nome = a.Nome,
-                                    NomeResponsavel = a.TipoResponsavel == TipoResponsavelEnum.Proprio_Aluno &&
-                                                        !string.IsNullOrWhiteSpace(a.NomeSocial) ?
-                                                        a.NomeSocial :
-                                                        a.NomeResponsavel,
-                                    CpfResponsavel = a.CpfResponsavel,
-                                    NomeSocial = a.NomeSocial,
-                                    DataNascimento = a.DataNascimento.Date,
-                                    CodigoTipoEscola = a.CodigoTipoEscola,
-                                    CodigoEscola = a.CodigoEscola,
-                                    DescricaoTipoEscola = a.DescricaoTipoEscola,
-                                    Escola = a.Escola,
-                                    CodigoDre = a.CodigoDre,
-                                    SiglaDre = a.SiglaDre,
-                                    CodigoTurma = a.CodigoTurma,
-                                    Turma = a.Turma,
-                                    SituacaoMatricula = a.SituacaoMatricula,
-                                    DataSituacaoMatricula = a.DataSituacaoMatricula,
-                                    SerieResumida = a.SerieResumida
-                                })
-                    });
 
-                return RespostaApi.Sucesso(tipoEscola);
+                return default;
+
+                //var turmasModalidade = await mediator.Send(new ObterTurmasModalidadesPorCodigos)
+
+
+
+                //dadosDosAlunos.ForEach(x => { var g = SelecionarGrupos(x.CodigoTipoEscola, x.CodigoEtapaEnsino, x.CodigoCicloEnsino, grupos); x.Grupo = g.gupo; x.CodigoGrupo = g.codigo; });
+
+                //var tipoEscola =
+                //    dadosDosAlunos
+                //    .GroupBy(g => new { g.Grupo, g.CodigoGrupo })
+                //    .Select(s => new ListaEscola
+                //    {
+                //        Grupo = s.Key.Grupo,
+                //        CodigoGrupo = s.Key.CodigoGrupo,
+                //        Alunos = dadosDosAlunos
+                //                .Where(w => w.CodigoGrupo == s.Key.CodigoGrupo)
+                //                .Select(a => new Dominio.Entidades.Aluno
+                //                {
+                //                    CodigoEol = a.CodigoEol,
+                //                    Nome = a.Nome,
+                //                    NomeResponsavel = a.TipoResponsavel == TipoResponsavelEnum.Proprio_Aluno &&
+                //                                        !string.IsNullOrWhiteSpace(a.NomeSocial) ?
+                //                                        a.NomeSocial :
+                //                                        a.NomeResponsavel,
+                //                    CpfResponsavel = a.CpfResponsavel,
+                //                    NomeSocial = a.NomeSocial,
+                //                    DataNascimento = a.DataNascimento.Date,
+                //                    CodigoTipoEscola = a.CodigoTipoEscola,
+                //                    CodigoEscola = a.CodigoEscola,
+                //                    DescricaoTipoEscola = a.DescricaoTipoEscola,
+                //                    Escola = a.Escola,
+                //                    CodigoDre = a.CodigoDre,
+                //                    SiglaDre = a.SiglaDre,
+                //                    CodigoTurma = a.CodigoTurma,
+                //                    Turma = a.Turma,
+                //                    SituacaoMatricula = a.SituacaoMatricula,
+                //                    DataSituacaoMatricula = a.DataSituacaoMatricula,
+                //                    SerieResumida = a.SerieResumida
+                //                })
+                //    });
+
+                //return RespostaApi.Sucesso(tipoEscola);
             }
 
             private (string gupo, long codigo) SelecionarGrupos(int? codigoTipoEscola, int codigoEtapaEnsino, int codigoCicloEnsino, IEnumerable<GrupoComunicado> grupos)
---
+++ src/SME.AE.Aplicacao/Consultas/ObterTurmasModalidadesPorCodigos/ObterTurmasModalidadesPorCodigosQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao
+{
+    public class ObterTurmasModalidadesPorCodigosQueryHandler : IRequestHandler<ObterTurmasModalidadesPorCodigosQuery, IEnumerable<TurmaModalidadeCodigoDto>>
+    {
+        public ObterTurmasModalidadesPorCodigosQueryHandler()
+        {
+
+        }
+        public Task<IEnumerable<TurmaModalidadeCodigoDto>> Handle(ObterTurmasModalidadesPorCodigosQuery request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
