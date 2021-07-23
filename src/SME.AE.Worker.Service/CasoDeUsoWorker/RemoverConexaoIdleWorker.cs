24f4c7263d3e3c2e0faa4e92f0ae9ee474c9c851
Luiz Henrique F. R. Araújo <luizhbd@gmail.com>
2021-07-23 11:45:18 -0300

    Removido comentário

--- src/SME.AE.Worker.Service/CasoDeUsoWorker/RemoverConexaoIdleWorker.cs
+++ src/SME.AE.Worker.Service/CasoDeUsoWorker/RemoverConexaoIdleWorker.cs
@@ -3,7 +3,6 @@ using System;
 
 namespace SME.AE.Worker.Service.CasoDeUsoWorker
 {
-    // [UseCaseWorker(CronParametroDB = "RemoverConexaoIdle", CronPadrao = "0 0 * 1 *")]
     public class RemoverConexaoIdleWorker : UseCaseWorker<RemoverConexaoIdleCasoDeUso>
     {
         public RemoverConexaoIdleWorker(IServiceProvider serviceProvider)
