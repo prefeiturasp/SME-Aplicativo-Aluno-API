cb85a51c097f0f21cb110893b4dd0f1852b1c2e9
Luiz Henrique F. R. Araújo <luizhbd@gmail.com>
2021-08-13 23:12:54 -0300

    alterado mapeamento do usuario notificacao

--- src/SME.AE.Infra/Persistencia/Mapeamentos/UsuarioNotificacaoMap.cs
+++ src/SME.AE.Infra/Persistencia/Mapeamentos/UsuarioNotificacaoMap.cs
@@ -4,5 +4,18 @@ namespace SME.AE.Infra.Persistencia.Mapeamentos
 {
     public class UsuarioNotificacaoMap : BaseMap<UsuarioNotificacao>
     {
+        public UsuarioNotificacaoMap()
+        {
+            ToTable("usuario_notificacao_leitura");
+            Map(a => a.UsuarioId).ToColumn("usuario_id");
+            Map(a => a.CodigoEolAluno).ToColumn("codigo_eol_aluno");
+            Map(a => a.CodigoEolAluno).ToColumn("codigo_eol_turma");
+            Map(a => a.NotificacaoId).ToColumn("notificacao_id");
+            Map(a => a.DreCodigoEol).ToColumn("dre_codigoeol");
+            Map(a => a.UeCodigoEol).ToColumn("ue_codigoeol");
+            Map(a => a.UsuarioCpf).ToColumn("usuario_cpf");
+            Map(a => a.MensagemVisualizada).ToColumn("mensagemvisualizada");
+            Map(a => a.MensagemExcluida).ToColumn("mensagemexcluida");
+        }
     }
 }
