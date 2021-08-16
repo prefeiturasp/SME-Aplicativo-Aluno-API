8fb23bf448c6bedc462b6362d5a5172d0805ac3e
Luiz Henrique F. R. Araújo <luizhbd@gmail.com>
2021-08-16 15:32:42 -0300

    ajuste para exclusão da notificacao do aluno

---
+++ src/SME.AE.Aplicacao/Comandos/Notificacao/RemoverNotificacaoAluno/RemoverNotificacaoAlunoPorNotificacoesIdsCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Comandos.Notificacao.Remover
+{
+    public class RemoverNotificacaoAlunoPorNotificacoesIdsCommandHandler : IRequestHandler<RemoverNotificacaoAlunoPorNotificacoesIdsCommand, bool>
+    {
+        private readonly INotificacaoAlunoRepositorio repositorio;
+
+        public RemoverNotificacaoAlunoPorNotificacoesIdsCommandHandler(INotificacaoAlunoRepositorio repositorio)
+        {
+            this.repositorio = repositorio ?? throw new System.ArgumentNullException(nameof(repositorio));
+        }
+
+
+        public async Task<bool> Handle(RemoverNotificacaoAlunoPorNotificacoesIdsCommand request, CancellationToken cancellationToken)
+        {
+            return await repositorio.RemoverPorNotificacoesIds(request.Ids);
+        }
+    }
+}
---
+++ src/SME.AE.Aplicacao/Comandos/Notificacao/RemoverNotificacaoTurma/RemoverNotificacaoTurmaPorNotificacoesIdsCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Comandos.Notificacao.Remover
+{
+    public class RemoverNotificacaoTurmaPorNotificacoesIdsCommandHandler : IRequestHandler<RemoverNotificacaoTurmaPorNotificacoesIdsCommand, bool>
+    {
+        private readonly INotificacaoTurmaRepositorio repositorio;
+
+        public RemoverNotificacaoTurmaPorNotificacoesIdsCommandHandler(INotificacaoTurmaRepositorio repositorio)
+        {
+            this.repositorio = repositorio ?? throw new System.ArgumentNullException(nameof(repositorio));
+        }
+
+
+        public async Task<bool> Handle(RemoverNotificacaoTurmaPorNotificacoesIdsCommand request, CancellationToken cancellationToken)
+        {
+            return await repositorio.RemoverPorNotificacoesIds(request.Ids);
+        }
+    }
+}
