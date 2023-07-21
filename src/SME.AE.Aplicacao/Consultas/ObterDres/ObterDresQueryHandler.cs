2e89b50cda91540ac932ad081c221c5f28790463
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-07-21 11:43:34 -0300

    99549 - Dres - Remoção de consulta direta ao banco do SGP e criado end point/api que consulta em base consulta SGP

---
+++ src/SME.AE.Aplicacao/Consultas/ObterDres/ObterDresQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using SME.AE.Aplicacao.Comum.Enumeradores;
+using SME.AE.Aplicacao.Comum.Interfaces.Repositorios;
+using SME.AE.Aplicacao.Comum.Modelos;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta;
+using SME.AE.Aplicacao.Comum.Modelos.Resposta.Dre;
+using SME.AE.Comum.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SME.AE.Aplicacao.Consultas
+{
+    public class ObterDresQueryHandler : IRequestHandler<ObterDresQuery, IEnumerable<DreResposta>>
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        public ObterDresQueryHandler(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory ?? throw new System.ArgumentNullException(nameof(httpClientFactory));
+        }
+        public async Task<IEnumerable<DreResposta>> Handle(ObterDresQuery request, CancellationToken cancellationToken)
+        {
+            var httpClient = httpClientFactory.CreateClient("servicoApiSgpChave");
+            var resposta = await httpClient.GetAsync($"v1/dres/integracoes");
+            if (resposta.IsSuccessStatusCode)
+            {
+                var json = await resposta.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<DreResposta>>(json);
+            }
+            else
+            {
+                throw new System.Exception($"Não foi possível obter as dres");
+            }
+        }
+    }
+}
