5d1f7e5679a86aa583f3e41907d6a05caad4069f
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-07-31 14:00:09 -0300

    100418 - Ajuste uso de appinsights removido na classe cache repository

--- src/SME.AE.Aplicacao/Servicos/ServicoLog.cs
+++ src/SME.AE.Aplicacao/Servicos/ServicoLog.cs
@@ -8,7 +8,6 @@ namespace SME.AE.Aplicacao.Servicos
 {
     public class ServicoLog : IServicoLog
     {
-        private readonly TelemetryClient insightsClient;
         private readonly string sentryDSN;
 
         public ServicoLog(IConfiguration configuration, TelemetryClient insightsClient)
@@ -18,7 +17,6 @@ namespace SME.AE.Aplicacao.Servicos
                 throw new ArgumentNullException(nameof(configuration));
             }
             sentryDSN = configuration.GetSection("Sentry:DSN").Value;
-            this.insightsClient = insightsClient ?? throw new ArgumentNullException(nameof(insightsClient));
         }
 
         public void Registrar(string mensagem)
@@ -35,20 +33,6 @@ namespace SME.AE.Aplicacao.Servicos
             {
                 SentrySdk.CaptureException(ex);
             }
-        }
-
-        public void RegistrarAppInsights(string evento, string mensagem)
-        {
-            insightsClient.TrackEvent(evento,
-                new Dictionary<string, string>()
-                          {
-                             { DateTime.Now.ToLongDateString(), mensagem }
-                         });
-        }
-
-        public void RegistrarDependenciaAppInsights(string tipoDependencia, string alvo, string mensagem, DateTimeOffset inicio, TimeSpan duracao, bool sucesso)
-        {
-            insightsClient.TrackDependency(tipoDependencia, alvo, mensagem, inicio, duracao, sucesso);
         }
     }
 }
